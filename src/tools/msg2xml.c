75e0f82099966307faccca39b7c2dfc34c1e64b2
Enno Rehling <enno@eressea.de>
2001-03-25 09:26:30 +0000

    xml-beispiel und message->xml converter

---
+++ src/tools/msg2xml.c
@@ -0,0 +1,227 @@
+#include <config.h>
+
+#include <stdio.h>
+#include <string.h>
+
+static struct vartype {
+	const char * name;
+	const char * type;
+	const char * msg;
+} vartype[] = {
+	{ "from", "unit", "donation"},
+
+	/* strange and to be changed */
+
+	{ "destruction", "int", "siege" },
+	{ "discover", "string", "givedumb" },
+	{ "receipient", "unit", "givecommand" },
+	{ "sink", "string", "entermaelstrom" },
+	{ "sink", "string", "storm" },
+	{ "using", "resource", "errusingpotion" },
+	{ "type", "string", "scunicorn" },
+	{ "mode", "string", "travel" },
+	{ "special", "string", "new_fspecial" },
+	{ "special", "string", "new_fspecial_level" },
+
+	/* broadband */
+
+	{ "regions", "string" },
+	{ "succ", "string" },
+	{ "renamed", "string" },
+	{ "reason", "string" },
+	{ "message", "string" },
+	{ "value", "string" },
+	{ "error", "string" },
+	{ "string", "string" },
+	{ "command", "string" },
+	{ "spell", "string" }, /* ? */
+
+	{ "building", "building" },
+
+	{ "ship", "ship" },
+
+	{ "resource", "resource" },
+	{ "potion", "resource" },
+	{ "item", "resource" },
+	{ "herb", "resource" },
+
+	{ "teacher", "unit" },
+	{ "student", "unit" },
+	{ "unit", "unit" },
+	{ "renamer", "unit" },
+	{ "spy", "unit" },
+	{ "mage", "unit" },
+	{ "opfer", "unit" },
+	{ "target", "unit" },
+	{ "recipient", "unit" },
+	{ "follower", "unit" },
+
+	{ "skill", "skill" },
+
+	{ "faction", "faction" },
+
+	{ "region", "region" },
+	{ "source", "region" },
+	{ "regionn", "region" },
+	{ "regionv", "region" },
+	{ "end", "region" },
+	{ "start", "region" },
+	{ "runto", "region" },
+	{ "to", "region" },
+	{ "from", "region" },
+
+	{ "kills", "int" },
+	{ "fallen", "int" },
+	{ "amount", "int" },
+	{ "aura", "int" },
+	{ "months", "int" },
+	{ "wanted", "int" },
+	{ "money", "int" },
+	{ "dead", "int" },
+	{ "level", "int" },
+	{ "days", "int" },
+	{ "damage", "int" },
+	{ "cost", "int" },
+	{ "want", "int" },
+	{ "size", "int" },
+	{ "alive", "int" },
+	{ "run", "int" },
+	{ "hits", "int" },
+	{ "turns", "int" },
+
+	{ "race", "race" },
+
+	{ "direction", "direction" },
+	{ "dir", "direction" },
+
+	{ "id", "int36" },
+	{ NULL, NULL }
+};
+
+static const char * 
+type(const char * name,const char * msg)
+{
+	int i = 0;
+	while (vartype[i].name) {
+		if (strcmp(name, vartype[i].name)==0 && 
+			(vartype[i].msg==NULL || strcmp(vartype[i].msg, msg)==0)) {
+			return vartype[i].type;
+		}
+		++i;
+	}
+	fprintf(stderr, "unknown type for \"%s\" in message \"%s\".\n", name, msg);
+	return "unknown";
+}
+
+static void
+parse_message(char * b, FILE * ostream)
+{
+	char *m, *a = NULL, message[8192];
+	char * name;
+	char * language;
+	char * section = NULL;
+	int i, level = 0;
+	char * args[16];
+	boolean f_symbol = false;
+
+	/* skip comments */
+	if (b[0]=='#' || b[0]==0) return;
+
+	/* the name of this type */
+	name = b;
+	while (*b && *b!=';') ++b;
+	if (!*b) return;
+	*b++ = 0;
+
+	/* the section for this type */
+	section = b;
+	while (*b && *b!=';' && *b!=':') ++b;
+	if (!strcmp(section, "none")) section=NULL;
+
+	/* if available, the level for this type */
+	if (*b==':') {
+		char * x;
+		*b++ = 0;
+		x = b;
+		while (*b && *b!=';') ++b;
+		level=atoi(x);
+	}
+	*b++ = 0;
+
+	/* the locale */
+	language = b;
+	while (*b && *b!=';') ++b;
+	*b++ = 0;
+
+	/* parse the message */
+	i = 0;
+	m = message;
+	*m++='\"';
+	while (*b) {
+		switch (*b) {
+		case '{':
+			f_symbol = true;
+			a = ++b;
+			break;
+		case '}':
+			*b++ = '\0';
+			args[i] = strdup(a);
+			sprintf(m, "$%s", args[i]);
+			m+=strlen(m);
+			i++;
+			f_symbol = false;
+			break;
+		case ' ':
+			if (f_symbol) {
+				a = ++b;
+				break;
+			}
+			/* fall-through intended */
+		default:
+			if (!f_symbol) {
+				*m++ = *b++;
+			} else b++;
+		}
+	}
+	strcpy(m, "\"");
+	args[i] = NULL;
+
+	/* add the messagetype */
+	fprintf(ostream, "<message name=\"%s\">\n", name);
+	for (i=0;args[i];++i) {
+		fprintf(ostream, "\t<param name=\"%s\" type=\"%s\"></param>\n", args[i], type(args[i], name));
+	}
+	fprintf(ostream, "\t<locale name=\"%s\">\n", language);
+	fprintf(ostream, "\t\t<nr section=\"%s\">\n", 
+		language, section);
+	fprintf(ostream, "\t\t\t<text>%s</text>\n", message);
+	fputs("\t\t</nr>\n", ostream);
+	fputs("\t</locale>\n", ostream);
+	fputs("</message>\n\n", ostream);
+}
+
+void
+read_messages(FILE * istream, FILE * ostream)
+{
+	char buf[8192];
+	fputs("<messages>\n", ostream);
+	while (fgets(buf, sizeof(buf), istream)) {
+		buf[strlen(buf)-1] = 0; /* \n weg */
+		parse_message(buf, ostream);
+	}
+	fputs("</messages>\n", ostream);
+}
+
+int 
+main(int argc, char** argv)
+{
+	FILE * istream = stdin;
+	FILE * ostream = stdout;
+	int nretval = -1;
+	if (argc>1) istream = fopen(argv[1], "rt+");
+	if (argc>2) ostream = fopen(argv[2], "wt+");
+	if (istream && ostream) {
+		read_messages(istream, ostream);
+	}
+	return nretval;
+}
---
+++ src/tools/xmltest.c
@@ -0,0 +1,55 @@
+#include <config.h>
+#include <xml.h>
+
+#include <stdio.h>
+#include <string.h>
+
+static int 
+cbplaintext(const struct xml_stack * stack, const char * c)
+{
+	puts(c);
+	fflush(stdout);
+	return XML_OK;
+}
+
+static int 
+cbtagend(const struct xml_stack * stack)
+{
+	xml_tag * tag = stack->tag;
+	printf("</%s>\n", tag->name);
+	fflush(stdout);
+	return XML_OK;
+}
+
+static int 
+cbtagbegin(const struct xml_stack * stack)
+{
+	xml_tag * tag = stack->tag;
+	xml_attrib * xa= tag->attribs;
+	printf("<%s", tag->name);
+	while (xa) {
+		printf(" %s=\"%s\"", xa->name, xa->value);
+		xa = xa->next;
+	}
+	printf(">\n");
+	fflush(stdout);
+	return XML_OK;
+}
+
+int 
+main(int argc, char** argv)
+{
+	FILE * istream = stdin;
+	int nretval = -1;
+	if (argc>1) istream = fopen(argv[1], "rt+");
+	if (istream) {
+		xml_callbacks xml_cb = { NULL };
+		xml_cb.plaintext = cbplaintext;
+		xml_cb.tagbegin = cbtagbegin;
+
+		nretval = xml_parse(istream, &xml_cb);
+		if (istream!=stdin) fclose(istream);
+		if (ostream!=stdout) fclose(ostream);
+	}
+	return nretval;
+}
