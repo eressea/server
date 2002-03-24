37bb05af95e46aaac9988ccdadb6f4a1ba06d18c
Enno Rehling <enno@eressea.de>
2002-03-24 12:11:59 +0000

    neue parteien in deutsch rausschreiben

--- scripts/register/getfactions.py
+++ scripts/register/getfactions.py
@@ -6,7 +6,7 @@ import sys
 dbname=sys.argv[1]
 maxnum=int(sys.argv[2])
 
-query = "select distinct u.email, s.race, u.locale from users u, subscriptions s left join userips i on u.id=i.user left join bannedips b on i.ip=b.ip where s.user=u.id and b.ip is NULL and u.status='CONFIRMED' order by u.id"
+query = "select distinct u.email, r.name, u.locale from users u, races r, subscriptions s left join userips i on u.id=i.user left join bannedips b on i.ip=b.ip where s.user=u.id and b.ip is NULL and u.status='CONFIRMED' and r.race=s.race and r.locale='de' order by u.id"
 db=MySQLdb.connect(db=dbname)
 cursor = db.cursor()
 num=cursor.execute(query)
