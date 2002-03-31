0f28866c0d3d463e583c56d0d2a9c3521b75b5e1
Enno Rehling <enno@eressea.de>
2002-03-31 07:47:18 +0000

    kleine änderung an der ausgabe

--- scripts/register/unpaid.sql
+++ scripts/register/unpaid.sql
@@ -1,7 +1,6 @@
-select distinct u.id ID, left(concat(firstname,' ',lastname, ' <',email,'>'),43) Name, sum(t.balance) Kontostand
+select distinct u.id ID, s.faction Partei, left(concat(firstname,' ',lastname, ' <',email,'>'),43) Name, sum(t.balance) Kontostand
        from users u, transactions t, subscriptions s
        where u.id=t.user and u.id=s.user
-	   		 and u.status!='CONFIRMED' 
 			 and s.status='ACTIVE'
-	   GROUP BY u.id
-	   HAVING SUM(t.balance)<2.5;
+	   GROUP BY s.faction
+	   HAVING Kontostand<2.5;
