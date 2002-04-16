09c322582a050e0c14d5ef9dde9d60a889b8fcf4
Enno Rehling <enno@eressea.de>
2002-04-16 16:01:52 +0000

    neue passwortmimik

--- scripts/register/getfactions.py
+++ scripts/register/getfactions.py
@@ -6,7 +6,7 @@ import sys
 dbname=sys.argv[1]
 maxnum=int(sys.argv[2])
 
-query = "select distinct u.email, r.name, u.locale, s.bonus from users u, races r, subscriptions s left join userips i on u.id=i.user left join bannedips b on i.ip=b.ip where s.user=u.id and b.ip is NULL and u.status='CONFIRMED' and r.race=s.race and r.locale='de' order by u.id"
+query = "select distinct u.email, s.password, r.name, u.locale, s.bonus from users u, races r, subscriptions s left join userips i on u.id=i.user left join bannedips b on i.ip=b.ip where s.user=u.id and b.ip is NULL and u.status='CONFIRMED' and r.race=s.race and r.locale='de' order by u.id"
 db=MySQLdb.connect(db=dbname)
 cursor = db.cursor()
 num=cursor.execute(query)
@@ -14,7 +14,7 @@ if num>maxnum:
     num=maxnum
 while num:
     num=num-1
-    email, race, locale, bonus = cursor.fetchone()
+    email, password, race, locale, bonus = cursor.fetchone()
     if bonus==None:
 	bonus=0
-    print email+" "+race+" "+locale+" "+str(int(bonus))
+    print email+" "+race+" "+locale+" "+str(int(bonus))+" "+password
