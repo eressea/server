94c596c388c8fb273d2074d73e42ebd76b1da5df
Enno Rehling <enno@eressea.de>
2005-02-18 13:47:37 +0000

    http://eressea.upb.de/mantis/view.php?id=444
    !isalnum statt isspace sollte den Parser weniger Anfaellig amchen fuer
    bekloppte Mailer.

--- src/common/kernel/order.c
+++ src/common/kernel/order.c
@@ -98,7 +98,7 @@ free_orders(order ** olist)
 order * 
 parse_order(const char * s, const struct locale * lang)
 {
-  while (isspace(*(unsigned char*)s)) ++s;
+  while (!isalnum(*(unsigned char*)s)) ++s;
   if (*s==0) return NULL;
   else {
     const char * sptr;
