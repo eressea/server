9133042ed456062ebd4ef2191f1abb3c4c219498
Enno Rehling <enno@eressea.de>
2005-07-25 13:04:17 +0000

    bugs, bugs, bugs

--- src/scripts/eressea-multis.lua
+++ src/scripts/eressea-multis.lua
@@ -1,10 +1,9 @@
 local function kill_multis()
   local multis = { "ogq", "idan", "e8L1" }
   for k, v in multis do
-    print(v)
     local f = get_faction(atoi36(v))
     if f~=nil then
-      print("Marking " .. tostring(f) .. " as a multi-player.")
+      print("- marking " .. tostring(f) .. " as a multi-player.")
       f.email="doppelspieler@eressea.de"
       f.password=""
     else
