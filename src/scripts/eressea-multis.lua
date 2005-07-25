4b58871100157e095eb58d885d4fdd0f81f7211c
Enno Rehling <enno@eressea.de>
2005-07-25 13:03:43 +0000

    Bugs, bugs, bugs...

--- src/scripts/eressea-multis.lua
+++ src/scripts/eressea-multis.lua
@@ -1,13 +1,17 @@
 local function kill_multis()
-  local multis = [ "ogq", "idan", "e8L1" ]
-  for key in multis do
-    local f = get_faction(atoi36(multis[key]))
+  local multis = { "ogq", "idan", "e8L1" }
+  for k, v in multis do
+    print(v)
+    local f = get_faction(atoi36(v))
     if f~=nil then
+      print("Marking " .. tostring(f) .. " as a multi-player.")
       f.email="doppelspieler@eressea.de"
       f.password=""
-      print "Marking " .. f .. " as a multi-player."
+    else
+      print("- could not find faction " .. v)
     end
   end
 end
 
+print("killing multi-players")
 kill_multis()
