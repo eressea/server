a3bef667d15bdfea0c85b46c210808b794d57ed9
Enno Rehling <enno@eressea.de>
2005-08-22 20:10:03 +0000

    initializing oldrace, sanity checks.

--- src/common/kernel/xmlreader.c
+++ src/common/kernel/xmlreader.c
@@ -524,6 +524,9 @@ race_compat(void)
       if (rc == new_race[RC_TROLL]) {
         a_add(&rc->attribs, make_skillmod(NOSKILL, SMF_RIDING, NULL, 0.0, -1));
       }
+    } else {
+      log_error(("could not find old race %s\n", oldracenames[i]));
+      assert(rc);
     }
   }
 }
