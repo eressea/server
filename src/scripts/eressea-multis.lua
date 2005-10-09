a461d06aa65d97f2bbb2dfee802a841d04716cf7
Enno Rehling <enno@eressea.de>
2005-10-09 09:10:55 +0000

    Disabling multiplayer-marking again.

--- src/scripts/eressea-multis.lua
+++ src/scripts/eressea-multis.lua
@@ -1,5 +1,5 @@
 local function kill_multis()
-  local multis = { "aLuk", "3", "rd2r", "aaa", "no3i", "1Lru", "heLd" }
+  local multis = {}
   for k, v in multis do
     local f = get_faction(atoi36(v))
     if f~=nil then
