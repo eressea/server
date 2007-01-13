97a2cbe5b00f806b65747881873a447ee7c1f01e
Enno Rehling <enno@eressea.de>
2007-01-13 21:55:11 +0000

    Effekt von Weihnachtsbaum, Regionsmeldungen.

--- src/scripts/eressea/xmas2006.lua
+++ src/scripts/eressea/xmas2006.lua
@@ -1,16 +1,22 @@
 function use_xmastree(u, amount)
   u.region:set_key("xm06", true)
   u:use_pooled("xmastree", amount)
-  return -1
+  msg = message("usepotion")
+  msg:set_unit("unit", u)
+  msg:set_resource("potion", "xmastree")
+  msg:send_region(u.region)
+  return 0
 end
 
 function update_xmas2006()
   if get_season == "winter" then
+    msg = message("xmastree_effect")
     for r in regions() do
       if r:get_key("xm06") then
         trees = r:get_resource("tree")
         if trees>0 then
           r:set_resource("tree", trees * 1.1)
+          msg:send_region(r)
         end
       end
     end
