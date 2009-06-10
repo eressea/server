faa7f6ac2703f50c964122f8fdf06f1134e3e901
Enno Rehling <enno@eressea.de>
2009-06-10 05:32:42 +0000

    - removed herbalism skill - allowed rep.xbow to halflings

--- src/scripts/e2k9/items.lua
+++ src/scripts/e2k9/items.lua
@@ -1,22 +1,27 @@
 -- when appending to this, make sure the item has a canuse-function!
 local goblin_denied = " plate lance mallornlance greatbow axe greatsword halberd rustyaxe rustyhalberd towershield "
 function item_canuse(u, iname)
-  if u.race=="goblin" then
+  local race = u.race
+  if race=="goblin" then
     if string.find(goblin_denied, " " .. iname .. " ") then
       return false
     end
   end
-  if iname=="towershield" or iname=="rep_crossbow" then
+  if iname=="rep_crossbow" then
+    -- only dwarves and halflings allowed to use towershield
+    return race=="dwarf" or race=="halfling"
+  end
+  if iname=="towershield" then 
     -- only dwarves allowed to use towershield
-    return u.race=="dwarf"
+    return race=="dwarf"
   end
   if iname=="greatbow" then
     -- only elves use greatbow
-    return u.race=="elf"
+    return race=="elf"
   end
   if iname=="plate" then
     -- goblins cannot use plate
-    return u.race~="goblin"
+    return race~="goblin"
   end
   return true
 end
