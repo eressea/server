c4742f5886bff2954b3f1d19618863709ac389ca
Enno Rehling <enno@eressea.de>
2010-02-24 16:16:01 +0000

    moving spells into eressea-only location (should this be eressea/src instead of just eressea?)

---
+++ src/eressea/spells/spells.h
@@ -0,0 +1,33 @@
+/* vi: set ts=2:
+ *
+ *
+ * Eressea PB(E)M host Copyright (C) 1998-2003
+ *      Christian Schlittchen (corwin@amber.kn-bremen.de)
+ *      Katja Zedel (katze@felidae.kn-bremen.de)
+ *      Henning Peters (faroul@beyond.kn-bremen.de)
+ *      Enno Rehling (enno@eressea.de)
+ *      Ingo Wilken (Ingo.Wilken@informatik.uni-oldenburg.de)
+ *
+ * This program may not be used, modified or distributed without
+ * prior permission by the authors of Eressea.
+ */
+
+#ifndef H_SPL_SPELLS
+#define H_SPL_SPELLS
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  struct ship;
+  struct curse;
+  struct unit;
+
+  extern void register_spells(void);
+
+  int levitate_ship(struct ship * sh, struct unit * mage, double power, int duration);
+  void set_spelldata(struct spell * sp);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/eressea/spells/unitcurse.h
@@ -0,0 +1,29 @@
+/* vi: set ts=2:
+ *
+ * Eressea PB(E)M host Copyright (C) 1998-2003
+ *      Christian Schlittchen (corwin@amber.kn-bremen.de)
+ *      Katja Zedel (katze@felidae.kn-bremen.de)
+ *      Henning Peters (faroul@beyond.kn-bremen.de)
+ *      Enno Rehling (enno@eressea.de)
+ *      Ingo Wilken (Ingo.Wilken@informatik.uni-oldenburg.de)
+ *
+ * This program may not be used, modified or distributed without
+ * prior permission by the authors of Eressea.
+ */
+
+#ifndef _UCURSE_H
+#define _UCURSE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct curse;
+struct message;
+extern struct message * cinfo_unit(const void * obj, typ_t typ, const struct curse *c, int self);
+
+extern void register_unitcurse(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _UCURSE_H */
