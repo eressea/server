ce4ad01f3a808812f6f0c9def8e87d419bcc5ed8
Enno Rehling <enno@eressea.de>
2010-02-24 02:48:39 +0000

    moving files that are exclusive to eressea & e3

--- src/scripts/setup.lua
+++ src/scripts/setup.lua
@@ -1,7 +1,8 @@
 local srcpath = config.source_dir
-local paths = { 'lunit/?.lua','external/lunit/?.lua','scripts/?.lua';'scripts/?' }
+local respath = srcpath .. '/' .. config.game .. '/res/'
+local paths = { config.game..'/scripts/?.lua';'lunit/?.lua','external/lunit/?.lua','scripts/?.lua';'scripts/?' }
 
-tests = {'common', config.game}
+tests = {'common'}
 for idx, test in pairs(tests) do
   tests[idx] = srcpath .. '/scripts/tests/' .. test .. '.lua'
 end
@@ -10,6 +11,6 @@ for idx, path in pairs(paths) do
   package.path = srcpath .. '/' .. path .. ';' .. package.path
 end
 
-read_xml(srcpath .. '/res/' .. config.game .. '.xml')
+read_xml(respath..'config-'..config.game..'.xml', respath..'catalog-'..config.game..'.xml')
 
 require "init"
