e6ccccf05474b935c6b8d6d2e7504824649face2
Enno Rehling <enno@eressea.de>
2011-02-19 06:17:08 -0800

    * move the bson tests
    * include the spells tests

--- scripts/e3a/modules.lua
+++ scripts/e3a/modules.lua
@@ -5,10 +5,11 @@ require "e3a.markets"
 
 local srcpath = config.source_dir
 tests = {
+  srcpath .. '/shared/scripts/tests/common.lua', 
+  srcpath .. '/shared/scripts/tests/spells.lua', 
+  srcpath .. '/shared/scripts/tests/bson.lua', 
   srcpath .. '/eressea/scripts/tests/castles.lua', 
   srcpath .. '/eressea/scripts/tests/morale.lua', 
-  srcpath .. '/shared/scripts/tests/common.lua', 
   srcpath .. '/eressea/scripts/tests/e3a.lua', 
   srcpath .. '/eressea/scripts/tests/stealth.lua', 
-  srcpath .. '/eressea/scripts/tests/bson.lua', 
 }
