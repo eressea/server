cf9bf54a95f6fd29c307d4e889a8a59f68325d7e
Enno Rehling <enno.rehling@gmail.com>
2011-02-26 23:21:12 -0800

    spells.lua is a bad file for example and meropis, it tests things that not every game has.
    lua_pushinteger() must be new, I've never used it before.
    all tests in meropis and example pass.

--- scripts/example/modules.lua
+++ scripts/example/modules.lua
@@ -3,6 +3,5 @@ require "example.rules"
 local srcpath = config.source_dir
 tests = {
   srcpath .. '/shared/scripts/tests/common.lua', 
-  srcpath .. '/shared/scripts/tests/spells.lua', 
   srcpath .. '/shared/scripts/tests/bson.lua', 
 }
