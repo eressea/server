a1af32895022c1afc3046297f8218007c8679573
Enno Rehling <enno.rehling@gmail.com>
2011-03-06 11:27:37 +0100

    added Atlantis ship types

--- scripts/example/modules.lua
+++ scripts/example/modules.lua
@@ -8,8 +8,9 @@ tests = {
 
 -- variables used in testsuite
 test = {  
-swordmakeskill = 3, -- weaponsmithing skill for swords
-swordiron = 1, -- iron per sword
-horsecapacity = 2000, -- carrying capacity of horses (minus weight)
-cartcapacity = 10000 -- carrying capacity of carts (minus weight)
+swordmakeskill = 3 -- weaponsmithing skill for swords
+,swordiron = 1 -- iron per sword
+,horsecapacity = 2000 -- carrying capacity of horses (minus weight)
+,cartcapacity = 10000 -- carrying capacity of carts (minus weight)
+,shipname = "boat" -- some ship name (any name)
 }
