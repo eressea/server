749d71f86d151b287b250893128ee45fd4c09267
Enno Rehling <enno.rehling@gmail.com>
2012-05-25 19:55:47 -0700

    testing spells in the example server also
    WIP: create_roi is broken until mages have spellbooks

--- scripts/example/modules.lua
+++ scripts/example/modules.lua
@@ -3,6 +3,7 @@ require "example.rules"
 local srcpath = config.source_dir
 tests = {
   srcpath .. '/example/scripts/tests/rules.lua', 
+  srcpath .. '/shared/scripts/tests/spells.lua', 
   srcpath .. '/shared/scripts/tests/common.lua', 
   srcpath .. '/shared/scripts/tests/bson.lua', 
 }
