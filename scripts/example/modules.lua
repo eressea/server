77be59a223c5bef727859737422206bcea247906
Enno Rehling <enno.rehling@gmail.com>
2012-05-29 18:08:56 -0700

    repair lua spells (bad stack index)
    add a whole bunch more spell tests
    allow spell tests to disable fumbles

--- scripts/example/modules.lua
+++ scripts/example/modules.lua
@@ -1,9 +1,10 @@
+require "spells"
 require "example.rules"
 
 local srcpath = config.source_dir
 tests = {
-  srcpath .. '/example/scripts/tests/rules.lua', 
   srcpath .. '/shared/scripts/tests/spells.lua', 
   srcpath .. '/shared/scripts/tests/common.lua', 
   srcpath .. '/shared/scripts/tests/bson.lua', 
+  srcpath .. '/example/scripts/tests/rules.lua', 
 }
