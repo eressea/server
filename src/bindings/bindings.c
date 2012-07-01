cb14492ccde24443b69974b8b2af5221ac934961
Enno Rehling <enno.rehling@gmail.com>
2012-07-01 22:31:14 +0200

    make cmake find the best Lua version that's installed

--- src/bindings/bindings.c
+++ src/bindings/bindings.c
@@ -1160,39 +1160,9 @@ int tolua_bindings_open(lua_State * L)
   return 1;
 }
 
-static const struct {
-  const char *name;
-  int (*func) (lua_State *);
-} lualibs[] = {
-  {
-  "", luaopen_base}, {
-  LUA_TABLIBNAME, luaopen_table}, {
-  LUA_IOLIBNAME, luaopen_io}, {
-  LUA_STRLIBNAME, luaopen_string}, {
-  LUA_MATHLIBNAME, luaopen_math}, {
-  LUA_LOADLIBNAME, luaopen_package}, {
-  LUA_DBLIBNAME, luaopen_debug},
-#if LUA_VERSION_NUM>=501
-  {
-  LUA_OSLIBNAME, luaopen_os},
-#endif
-  {
-  NULL, NULL}
-};
-
 static void openlibs(lua_State * L)
 {
   luaL_openlibs(L);
-/*  int i, err;
-  for (i = 0; lualibs[i].func; ++i) {
-    lua_pushcfunction(L, lualibs[i].func);
-    lua_pushstring(L, lualibs[i].name);
-    err = lua_pcall(L, 1, 0, 0);
-    if (err != 0) {
-      log_lua_error(L);
-    }
-  }
-*/
 }
 
 void lua_done(lua_State * L) {
