9d8e02d094b2392f7e2f3c35efda982df6895839
Enno Rehling <enno.rehling@gmail.com>
2012-07-01 18:00:31 -0700

    test that movement into coastal regions is blocked when no harbor exists, unless ship is allowed in this terrain.

--- src/tests.h
+++ src/tests.h
@@ -39,6 +39,9 @@ extern "C" {
   struct building * test_create_building(struct region * r, const struct building_type * btype);
   struct ship * test_create_ship(struct region * r, const struct ship_type * stype);
   struct item_type * test_create_itemtype(const char ** names);
+  struct ship_type *test_create_shiptype(const char **names);
+  struct building_type *test_create_buildingtype(const char *name);
+
   int RunAllTests(void);
 
 #ifdef __cplusplus
