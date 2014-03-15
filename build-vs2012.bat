60a7407a79d81d073a102d1ce6785c238802bb9d
Enno Rehling <enno.rehling@gmail.com>
2014-03-15 12:06:19 -0700

    CLI build script for Visual Studio 2012
    This is just a test, isn't working yet.

---
+++ build-vs2012.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+mkdir build-vs11
+cd build-vs11
+"c:\Program Files (x86)\CMake 2.8\bin\cmake.exe" -G "Visual Studio 11" -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/Lua/5.1":../../dependencies-win32 -DCMAKE_MODULE_PATH=../cmake/Modules ..
