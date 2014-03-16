82aeb4b2419240a326057e3c376520e14161c3a5
Enno Rehling <enno.rehling@gmail.com>
2014-03-15 22:02:49 -0700

    fix windows slashes
    cmake doesn't like backslashes, but has the tools to fix them
    added a build script for VS2013 (Version 12)

--- vs2010-build.bat
+++ vs2010-build.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
-mkdir build-vs10
-cd build-vs10
-SET ERESSEA=%Userprofile%/Documents/Projects/Eressea
-"%ProgramFiles(x86)%\CMake 2.8\bin\cmake.exe" -G "Visual Studio 10" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%ERESSEA%/server/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
+SET VSVERSION=10
+SET ERESSEA=%Userprofile%/Documents/Eressea
+
+mkdir build-vs%VSVERSION%
+cd build-vs%VSVERSION%
+"%ProgramFiles(x86)%\CMake 2.8\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%ERESSEA%/server/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
--- vs2012-build.bat
+++ vs2012-build.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
-mkdir build-vs11
-cd build-vs11
-"c:\Program Files (x86)\CMake 2.8\bin\cmake.exe" -G "Visual Studio 11" -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/Lua/5.1;C:/Users/Enno/Documents/Eressea/dependencies-win32" -DCMAKE_MODULE_PATH="C:/Users/Enno/Documents/Eressea/server/cmake/Modules" ..
+SET VSVERSION=11
+SET ERESSEA=%Userprofile%/Documents/Eressea
+
+mkdir build-vs%VSVERSION%
+cd build-vs%VSVERSION%
+"%ProgramFiles(x86)%\CMake 2.8\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%ERESSEA%/server/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
