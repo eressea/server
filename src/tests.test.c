9d24c3c291e1a408fdfffe2ccaafb653b5f2014b
Enno Rehling <enno.rehling@gmail.com>
2014-06-24 19:51:06 -0700

    testing resource caching

--- src/tests.test.c
+++ src/tests.test.c
@@ -9,6 +9,24 @@
 #include <CuTest.h>
 #include <tests.h>
 
+static void test_resources(CuTest *tc) {
+    const char *names[] = { "horse", "horse_p" };
+    resource_type *rtype;
+    test_cleanup();
+    CuAssertPtrEquals(tc, 0, rt_find("horse"));
+    rtype = new_resourcetype(names, 0, 0);
+    CuAssertPtrEquals(tc, 0, (void *)get_resourcetype(R_HORSE));
+    rt_register(rtype);
+    CuAssertPtrEquals(tc, (void *)rtype, (void *)rt_find("horse"));
+    CuAssertPtrEquals(tc, (void *)rtype, (void *)get_resourcetype(R_HORSE));
+    test_cleanup();
+    CuAssertPtrEquals(tc, 0, rt_find("horse"));
+    rtype = new_resourcetype(names, 0, 0);
+    CuAssertPtrEquals(tc, 0, (void *)get_resourcetype(R_HORSE));
+    rt_register(rtype);
+    CuAssertPtrEquals(tc, (void *)rtype, (void *)get_resourcetype(R_HORSE));
+}
+
 static void test_recreate_world(CuTest * tc)
 {
   test_cleanup();
@@ -50,6 +68,7 @@ static void test_recreate_world(CuTest * tc)
 CuSuite *get_tests_suite(void)
 {
   CuSuite *suite = CuSuiteNew();
+  SUITE_ADD_TEST(suite, test_resources);
   SUITE_ADD_TEST(suite, test_recreate_world);
   return suite;
 }
