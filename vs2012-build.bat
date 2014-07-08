22532969242c3da78aa788f6d0051047bef0dbc1
Enno Rehling <enno.rehling@gmail.com>
2014-07-08 00:00:03 -0700

    CMAke 3.0 has been released, and it installs to a new path.

--- vs2012-build.bat
+++ vs2012-build.bat
@@ -6,4 +6,4 @@ SET ERESSEA=%CD%
 
 mkdir build-vs%VSVERSION%
 cd build-vs%VSVERSION%
-"%ProgramFiles(x86)%\CMake 2.8\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%ERESSEA%/server/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
+"%ProgramFiles(x86)%\CMake\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%ERESSEA%/server/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
