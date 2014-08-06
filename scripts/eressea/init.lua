7348ce698bafc7cfde7ed74f800b21124550dacb
Enno Rehling <enno.rehling@gmail.com>
2014-08-06 23:47:16 +0200

    add config.path parsing to a module of its own.

--- scripts/eressea/init.lua
+++ scripts/eressea/init.lua
@@ -1,10 +1,4 @@
-if config.paths ~= nil then
-    for path in string.gmatch(config.paths, "([^:]+)") do
-        package.path = package.path .. ';' .. path .. '/?.lua;' .. path .. '/?/init.lua'
-    end
-end
-print(package.path)
-
+require 'eressea.path'
 require 'eressea.resources'
 require 'eressea.spells'
 
