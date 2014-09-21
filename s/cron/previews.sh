c5be92a7fcffcba4860999542131b9ffd2cecb98
Enno Rehling <enno.rehling@gmail.com>
2014-09-21 10:03:48 +0200

    fix preview scripts to auto-checkout master

--- s/cron/previews.sh
+++ s/cron/previews.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-cd $ERESSEA/git
-s/preview build
-s/preview version
+[ -z $ERESSEA ] && ERESSEA=$HOME/eressea
+SRC=$ERESSEA/git
+$SRC/s/preview build master
+$SRC/s/preview version
 for game in 2 3 4 ; do
-	s/preview -g $game run && \
-	s/preview -g $game send
+	$SRC/s/preview -g $game run && \
+	$SRC/s/preview -g $game send
 done
