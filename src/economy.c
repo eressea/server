f3e54e103a566e635270b59f3cddf1bdfb94df01
Enno Rehling <enno.rehling@gmail.com>
2014-10-25 23:54:01 +0200

    fix a crash when selling nothing. build 687

--- src/economy.c
+++ src/economy.c
@@ -2389,7 +2389,7 @@ static bool sell(unit * u, request ** sellorders, struct order *ord)
         }
     }
     else {
-        n = atoi((const char *)s);
+        n = s ? atoi(s) : 0;
         if (n == 0) {
             cmistake(u, ord, 27, MSG_COMMERCE);
             return false;
