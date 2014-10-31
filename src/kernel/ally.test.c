cf110d7788eff7a581f2ea362df410c1f859077a
Enno Rehling <enno.rehling@gmail.com>
2014-10-31 15:38:37 +0100

    fix allies with null faction (to be resolved later).

--- src/kernel/ally.test.c
+++ src/kernel/ally.test.c
@@ -7,22 +7,36 @@
 
 static void test_ally(CuTest * tc)
 {
-  ally * al = 0;
-  struct faction * f1 = test_create_faction(0);
+    ally * al = 0;
+    struct faction * f1 = test_create_faction(0);
 
-  ally_add(&al, f1);
-  CuAssertPtrNotNull(tc, al);
-  CuAssertPtrEquals(tc, f1, ally_find(al, f1)->faction);
+    ally_add(&al, f1);
+    CuAssertPtrNotNull(tc, al);
+    CuAssertPtrEquals(tc, f1, ally_find(al, f1)->faction);
 
-  ally_remove(&al, f1);
-  CuAssertPtrEquals(tc, 0, al);
-  CuAssertPtrEquals(tc, 0, ally_find(al, f1));
+    ally_remove(&al, f1);
+    CuAssertPtrEquals(tc, 0, al);
+    CuAssertPtrEquals(tc, 0, ally_find(al, f1));
+}
+
+static void test_ally_null(CuTest * tc)
+{
+    ally *a1 = 0, *a2 = 0;
+
+    a1 = ally_add(&a1, 0);
+    a2 = ally_add(&a1, 0);
+    CuAssertPtrNotNull(tc, a1);
+    CuAssertPtrNotNull(tc, a2);
+    CuAssertPtrEquals(tc, a2, a1->next);
+    CuAssertPtrEquals(tc, 0, a2->next);
+    free(a1);
+    free(a2);
 }
 
 CuSuite *get_ally_suite(void)
 {
-  CuSuite *suite = CuSuiteNew();
-  SUITE_ADD_TEST(suite, test_ally);
-  return suite;
+    CuSuite *suite = CuSuiteNew();
+    SUITE_ADD_TEST(suite, test_ally);
+    return suite;
 }
 
