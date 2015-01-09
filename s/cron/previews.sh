88fed0b418b40526ef86d60fec609b116182d717
Enno Rehling <enno.rehling@gmail.com>
2015-01-09 20:25:04 +0100

    build previews from the 3.4 branch

--- s/cron/previews.sh
+++ s/cron/previews.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 [ -z $ERESSEA ] && ERESSEA=$HOME/eressea
+
+branch=master
+if [ ! -n "$1" ]; then
+branch="$1"
+fi
 SRC=$ERESSEA/git
-$SRC/s/preview build master
+$SRC/s/preview build $branch
 $SRC/s/preview version
 for game in 2 3 4 ; do
 	$SRC/s/preview -g $game run && \
