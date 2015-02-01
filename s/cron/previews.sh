b06efdfa75b3c1c99f74924b471902a4d271415c
Enno Rehling <enno.rehling@gmail.com>
2015-02-01 08:37:04 +0100

    I cannot get this to work, hacking it

--- s/cron/previews.sh
+++ s/cron/previews.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-function abort() {
-echo $1
-[ -z $2 ] && exit -1
-exit $2 # otherwise
-}
-
 [ -z ${ERESSEA} ] && ERESSEA=~/eressea
 branch="master"
 if [ -e ${ERESSEA}/build/.preview ]; then
     branch=`cat ${ERESSEA}/build/.preview`
 fi
 SRC=${ERESSEA}/git
-${SRC}/s/preview build ${branch} || abort "failed to build ${branch}"
+${SRC}/s/preview build ${branch} || exit $?
 ${SRC}/s/preview version
 for game in 2 3 4 ; do
 	${SRC}/s/preview -g ${game} run && \
