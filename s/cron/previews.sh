6b1285a784772616937e525529b58661b3999880
Enno Rehling <enno.rehling@gmail.com>
2015-02-01 08:21:07 +0100

    fix preview script so it can checkout and build from tags

--- s/cron/previews.sh
+++ s/cron/previews.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+
+function abort() {
+echo $1
+[ -z $2 ] && exit -1
+exit $2 # otherwise
+}
+
 [ -z ${ERESSEA} ] && ERESSEA=~/eressea
 branch="master"
 if [ -e ${ERESSEA}/build/.preview ]; then
     branch=`cat ${ERESSEA}/build/.preview`
 fi
 SRC=${ERESSEA}/git
-${SRC}/s/preview build ${branch}
+${SRC}/s/preview build ${branch} || abort "failed to build ${branch}"
 ${SRC}/s/preview version
 for game in 2 3 4 ; do
 	${SRC}/s/preview -g ${game} run && \
