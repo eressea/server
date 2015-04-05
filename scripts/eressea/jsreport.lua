4fb0c133e8b488273f244b317871eb6f276985f5
Enno Rehling <enno.rehling@gmail.com>
2015-04-05 12:50:10 +0200

    enable json reports for develop branch

--- scripts/eressea/jsreport.lua
+++ scripts/eressea/jsreport.lua
@@ -1,14 +1,20 @@
 local pkg = {}
 
+print("loading jsreport module")
+
+function pkg.init()
+    eressea.settings.set("feature.jsreport.enable", "1")
+end
+
 function pkg.update()
     local factions = { '777', '1wpy', 'd08a', 'hani', 'scaL' }
-    for id in ipairs(factions) do
+    for _, id in ipairs(factions) do
         local f = faction.get(id)
         if f then
             local o = f.options
             local bit = (math.floor(o / 8) % 2)
             if bit==0 then
-                eressea.log.debug("enable JSON report for " .. tostring(f))
+                eressea.log.warning("enable JSON report for " .. tostring(f))
                 f.options = o + 8
             end
         end
