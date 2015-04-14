ae9fb399f30b0e71b01fd47de9b94cbe04f329ae
Enno Rehling <enno.rehling@gmail.com>
2015-04-14 02:32:01 +0200

    unit tests walkingcapacity, configurable trollbelt effect

--- src/move.h
+++ src/move.h
@@ -40,6 +40,7 @@ extern "C" {
 #define MV_WALK           (1<<9)        /* kann über Land gehen */
 
 #define HORSESNEEDED    2
+#define STRENGTHMULTIPLIER  50   /* multiplier for trollbelt */
 
     /* ein mensch wiegt 10, traegt also 5, ein pferd wiegt 50, traegt also 20. ein
     ** wagen wird von zwei pferden gezogen und traegt total 140, davon 40 die
