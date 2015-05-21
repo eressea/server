1c4778e3efa015babbaca4a31e85431d64e6ab61
Enno Rehling <enno.rehling@gmail.com>
2015-05-21 11:05:24 +0200

    abort tests when non-zero exit-code is returned, fix GiveRestriction for pool tests

--- scripts/tests/pool.lua
+++ scripts/tests/pool.lua
@@ -8,6 +8,7 @@ function setup()
     eressea.settings.set("rules.economy.food", "0")
     eressea.settings.set("nmr.removenewbie", "0")
     eressea.settings.set("nmr.timeout", "0")
+    eressea.settings.set("GiveRestriction", "0")
     eressea.settings.set("rules.magic.playerschools", "")
     conf = [[{
         "races": {
