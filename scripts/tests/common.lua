8afbb9a386814c8b883177737fc535c5a51d29f3
Enno Rehling <enno.rehling@gmail.com>
2015-07-01 20:44:17 +0200

    move offending test into a separate file.

--- scripts/tests/common.lua
+++ scripts/tests/common.lua
@@ -22,7 +22,7 @@ local function two_factions()
   return f1, f2
 end
 
-module("tests.eressea.common", package.seeall, lunit.testcase)
+module("tests.common", package.seeall, lunit.testcase)
 
 function setup()
     eressea.free_game()
@@ -600,33 +600,6 @@ function test_control()
     process_orders()
     assert_equal(u2, b.owner)
 end
--- segfault below
---[[
-
-function test_store_unit()
-    local r = region.create(0, 0, "plain")
-    local f = faction.create("noreply15@eressea.de", "human", "de")
-    local u = unit.create(f, r, 1)
-    local fid = f.id
-    u:add_item("money", u.number * 100)
-    local filename = config.basepath .. "/data/test.dat"
-    store = storage.create(filename, "wb")
-    assert_not_equal(store, nil)
-    store:write_unit(u)
-    store:close()
-    eressea.free_game()
-    -- recreate world:
-    r = region.create(0, 0, "plain")
-    f = faction.create("noreply16@eressea.de", "human", "de")
-    f.id = fid
-    store = storage.create(filename, "rb")
-    assert_not_nil(store)
-    u = store:read_unit()
-    store:close()
-    assert_not_nil(u)
-    assert_equal(u:get_item("money"), u.number * 100)
-end
-]]--
 
 function test_building_other()
     local r = region.create(0,0, "plain")
