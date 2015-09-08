2e0482973ece1d0644c88d7aa7c47fcd638324b9
Enno Rehling <enno.rehling@gmail.com>
2015-09-08 10:17:01 +0200

    testing test_seen_interval

--- src/seen.test.c
+++ src/seen.test.c
@@ -8,12 +8,22 @@
 #include <tests.h>
 
 static void test_seen_region(CuTest *tc) {
-    region *r;
+    region *r, *first, *last;
     seen_region **seen;
+    int dir;
 
     test_cleanup();
     r = test_create_region(0, 0, 0);
+    for (dir=0;dir!=MAXDIRECTIONS;++dir) {
+        region *rn = test_create_region(delta_x[dir], delta_y[dir], 0);
+    }
     seen = seen_init();
+    add_seen(seen, r, see_unit, true);
+    first = r;
+    last = 0;
+    get_seen_interval(seen, &first, &last);
+    CuAssertPtrEquals(tc, r, first);
+    CuAssertPtrEquals(tc, 0, last);
     seen_done(seen);
     test_cleanup();    
 }
