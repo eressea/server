42ae086def9db60d466dd4b824a2dc35b78dfd8f
Enno Rehling <enno.rehling@gmail.com>
2015-11-02 14:49:48 +0100

    unit is inisde a building also when it was not paid for

--- src/kernel/unit.c
+++ src/kernel/unit.c
@@ -1072,7 +1072,7 @@ void transfermen(unit * u, unit * dst, int n)
 
 struct building *inside_building(const struct unit *u)
 {
-    if (!u->building || !building_is_active(u->building)) {
+    if (!u->building) {
         return NULL;
     }
     else if (u->building->size < u->building->type->maxsize) {
