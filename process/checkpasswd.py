64937fac3a7352f2b0128f30f3b32c6e7a498b8e
Enno Rehling <enno@kn-bremen.de>
2016-02-11 20:39:56 +0100

    do all password checks with EPasswd

--- process/checkpasswd.py
+++ process/checkpasswd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, re
+from epasswd import EPasswd
 
 if len(sys.argv)<4:
     sys.exit(-2)
@@ -11,18 +12,9 @@ mypasswd=sys.argv[3]
 
 if mypasswd[0]=='"':
     mypasswd=mypasswd[1:len(mypasswd)-1]
-try:
-    infile = open(passfile, "r")
-except:
-    print "failed to open " + passfile
-    sys.exit(-1)
 
-for line in infile.readlines():
-    match = line.split(":")
-    if match!=None:
-        faction, passwd, override = match[0:3]
-        if faction==myfaction:
-            if passwd==mypasswd or override==mypasswd:
-                sys.exit(0)
+pw_data=EPasswd(passfile)
+if pw_data.fac_exists(myfaction):
+    if pw_data.check(myfaction, mypasswd):
+        sys.exit(0)
 sys.exit(-1)
-
