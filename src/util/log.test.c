cdd7606f024ac08557265fc8ea55b38a0cd832bc
Enno Rehling <enno.rehling@gmail.com>
2016-05-20 22:38:18 +0200

    fixing the password saving test.

--- src/util/log.test.c
+++ src/util/log.test.c
@@ -1,3 +1,4 @@
+#include <platform.h>
 #include <CuTest.h>
 
 #include "log.h"
--- vs2015-build.bat
+++ vs2015-build.bat
@@ -5,7 +5,9 @@ CD ..
 SET ERESSEA=%CD%
 
 CD %SRCDIR%
+IF exist build-vs%VSVERSION% goto HAVEDIR
 mkdir build-vs%VSVERSION%
+:HAVEDIR
 cd build-vs%VSVERSION%
 "%ProgramFiles(x86)%\CMake\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%SRCDIR%/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
 PAUSE
