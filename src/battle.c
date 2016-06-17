40dbe2b0cb108ac3efce74406d8ea11f5b6f0aef
Enno Rehling <enno.rehling@gmail.com>
2016-06-17 21:49:31 +0200

    delete old healing effects on read remove obsolete effect-handling from battle.c

--- src/battle.c
+++ src/battle.c
@@ -1323,11 +1323,8 @@ terminate(troop dt, troop at, int type, const char *damage, bool missile)
     }
 
     if (oldpotiontype[P_HEAL] && !fval(&df->person[dt.index], FL_HEALING_USED)) {
-        if ((get_effect(du, oldpotiontype[P_HEAL]) || i_get(du->items, oldpotiontype[P_HEAL]->itype) > 0)) {
-            if (get_effect(du, oldpotiontype[P_HEAL]))
-                change_effect(du, oldpotiontype[P_HEAL], -1);
-            else
-                i_change(&du->items, oldpotiontype[P_HEAL]->itype, -1);
+        if (i_get(du->items, oldpotiontype[P_HEAL]->itype) > 0) {
+            i_change(&du->items, oldpotiontype[P_HEAL]->itype, -1);
             message *m = msg_message("battle::potionsave", "unit", du);
             message_faction(b, du->faction, m);
             msg_release(m);
