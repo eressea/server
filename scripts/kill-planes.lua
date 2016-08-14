702a217080c3c361b67173f3e59590524341458e
Enno Rehling <enno.rehling@gmail.com>
2016-08-14 14:47:56 +0200

    fix glacier herbs (again) add a plane for the lost Regatta regions before killing it.

--- scripts/kill-planes.lua
+++ scripts/kill-planes.lua
@@ -13,7 +13,8 @@ require 'eressea.xmlconf'
 require 'eressea.path'
 
 eressea.read_game(get_turn() .. '.dat')
-ids = {2081501646, 1967748303, 1137, 2000, 1456894557, 1580742069, 1143084084, 285224813, 604912520, 296884068, 50}
+p=plane.create(50, -7278, -4493, 83,83, "Regatta")
+ids = {50, 2081501646, 1967748303, 1137, 2000, 1456894557, 1580742069, 1143084084, 285224813, 604912520, 296884068}
 
 for k,v in ipairs(ids) do
     p = plane.get(v)
