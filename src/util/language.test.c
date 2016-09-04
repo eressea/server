19d490ce71fd8c5cc6db7b2a1c2fec585f599a14
Enno Rehling <enno.rehling@gmail.com>
2016-09-04 22:07:52 +0200

    test_create_locale leaks memory. adding a simple test that shows this (under valgrind).

--- src/util/language.test.c
+++ src/util/language.test.c
@@ -5,10 +5,15 @@
 #include <CuTest.h>
 #include <tests.h>
 
+extern const char *directions[];
+
 static void test_language(CuTest *tc)
 {
+    const char *str;
     test_setup();
-
+    default_locale = test_create_locale();
+    str = directions[1];
+    CuAssertStrEquals(tc, str, locale_getstring(default_locale, str));
     test_cleanup();
 }
 
