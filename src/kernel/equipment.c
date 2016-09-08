993af3a7f973670a6cdb2a00fa144030d02a4cf8
Enno Rehling <enno.rehling@gmail.com>
2016-09-08 06:56:16 +0200

    memory leak in equipment.skills

--- src/kernel/equipment.c
+++ src/kernel/equipment.c
@@ -227,6 +227,7 @@ void equip_items(struct item **items, const struct equipment *eq)
 void equipment_done(void) {
     equipment **eqp = &equipment_sets;
     while (*eqp) {
+        int i;
         equipment *eq = *eqp;
         *eqp = eq->next;
         free(eq->name);
@@ -240,7 +241,10 @@ void equipment_done(void) {
             free(eq->items);
             eq->items = next;
         }
-        // TODO: subsets
+        // TODO: subsets, skills
+        for (i=0;i!=MAXSKILLS;++i) {
+            free(eq->skills[i]);
+        }
         free(eq);
     }
 }
