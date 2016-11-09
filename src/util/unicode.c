7d79822aa40d626a6ccf5990de2ead7abb5fef0f
Enno Rehling <enno@eressea.de>
2016-11-09 11:54:34 +0100

    fix missing nul-terminator.

--- src/util/unicode.c
+++ src/util/unicode.c
@@ -60,6 +60,10 @@ int unicode_utf8_mkname(utf8_t * op, size_t outlen, const utf8_t * ip)
         }
         ip += size;
     }
+    if (outlen <= 0) {
+        return ENOMEM;
+    }
+    *op = 0;
     return 0;
 }
 
