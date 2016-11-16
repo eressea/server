4a667326371761c32ff0a9a1ec78f759727f5e2f
Enno Rehling <enno.rehling@gmail.com>
2016-11-16 20:41:48 +0100

    renumber tests for building, ship

--- src/renumber.test.c
+++ src/renumber.test.c
@@ -3,6 +3,8 @@
 #include <tests.h>
 #include <kernel/unit.h>
 #include <kernel/faction.h>
+#include <kernel/building.h>
+#include <kernel/ship.h>
 #include <kernel/order.h>
 #include <util/base36.h>
 #include <util/language.h>
@@ -27,6 +29,40 @@ static void test_renumber_faction(CuTest *tc) {
     test_cleanup();
 }
 
+static void test_renumber_building(CuTest *tc) {
+    unit *u;
+    int uno, no;
+    const struct locale *lang;
+
+    test_setup_ex(tc);
+    u = test_create_unit(test_create_faction(0), test_create_region(0, 0, 0));
+    u->building = test_create_building(u->region, 0);
+    no = u->building->no;
+    uno = (no > 1) ? no - 1 : no + 1;
+    lang = u->faction->locale;
+    u->thisorder = create_order(K_NUMBER, lang, "%s %s", LOC(lang, parameters[P_BUILDING]), itoa36(uno));
+    renumber_cmd(u, u->thisorder);
+    CuAssertIntEquals(tc, uno, u->building->no);
+    test_cleanup();
+}
+
+static void test_renumber_ship(CuTest *tc) {
+    unit *u;
+    int uno, no;
+    const struct locale *lang;
+
+    test_setup_ex(tc);
+    u = test_create_unit(test_create_faction(0), test_create_region(0, 0, 0));
+    u->ship = test_create_ship(u->region, 0);
+    no = u->ship->no;
+    uno = (no > 1) ? no - 1 : no + 1;
+    lang = u->faction->locale;
+    u->thisorder = create_order(K_NUMBER, lang, "%s %s", LOC(lang, parameters[P_SHIP]), itoa36(uno));
+    renumber_cmd(u, u->thisorder);
+    CuAssertIntEquals(tc, uno, u->ship->no);
+    test_cleanup();
+}
+
 static void test_renumber_unit(CuTest *tc) {
     unit *u;
     int uno, no;
@@ -48,6 +84,8 @@ CuSuite *get_renumber_suite(void)
 {
     CuSuite *suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, test_renumber_unit);
+    SUITE_ADD_TEST(suite, test_renumber_building);
+    SUITE_ADD_TEST(suite, test_renumber_ship);
     SUITE_ADD_TEST(suite, test_renumber_faction);
     return suite;
 }
