2de3c3179a1be88f83e0415187f23976791ab866
Enno Rehling <enno@kn-bremen.de>
2016-11-28 08:39:07 +0100

    develop branch is now base for 3.11

--- src/kernel/version.c
+++ src/kernel/version.c
@@ -5,7 +5,7 @@
 
 #ifndef ERESSEA_VERSION
 // the version number, if it was not passed to make with -D
-#define ERESSEA_VERSION "3.10.0-devel"
+#define ERESSEA_VERSION "3.11.0-devel"
 #endif
 
 const char *eressea_version(void) {
