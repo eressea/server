b8bccd723845ea0d55e670dbd9487ea861004e76
Enno Rehling <enno.rehling@gmail.com>
2016-12-31 20:17:02 +0100

    monster name pluralization changes with u->number.

--- src/names.test.c
+++ src/names.test.c
@@ -42,12 +42,18 @@ static void test_monster_names(CuTest *tc) {
 
     test_setup();
     register_names();
+    default_locale = test_create_locale();
+    locale_setstring(default_locale, "race::irongolem", "Eisengolem");
+    locale_setstring(default_locale, "race::irongolem_p", "Eisengolems");
     rc = test_create_race("irongolem");
     u = test_create_unit(test_create_faction(rc), test_create_region(0, 0, 0));
     CuAssertPtrNotNull(tc, u->_name);
     rc->generate_name = (race_name_func)get_function("namegeneric");
     rc->generate_name(u);
     CuAssertPtrEquals(tc, 0, u->_name);
+    CuAssertStrEquals(tc, "Eisengolem", unit_getname(u));
+    u->number = 2;
+    CuAssertStrEquals(tc, "Eisengolems", unit_getname(u));
     test_cleanup();
 }
 
