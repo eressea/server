f9fbe607663052f47a2179b89f6c96ca716995c3
Enno Rehling <enno.rehling@gmail.com>
2017-01-06 20:54:57 +0100

    add a happy test for mt_new. refactor to not use strncpy.

---
+++ src/util/message.test.c
@@ -0,0 +1,29 @@
+#include <platform.h>
+#include "message.h"
+
+#include <CuTest.h>
+#include <tests.h>
+
+static void test_mt_new(CuTest *tc)
+{
+    message_type *mt;
+    test_setup();
+    mt = mt_new_va("test", "name:string", "number:int", NULL);
+    CuAssertPtrNotNull(tc, mt);
+    CuAssertStrEquals(tc, "test", mt->name);
+    CuAssertIntEquals(tc, 2, mt->nparameters);
+    CuAssertPtrNotNull(tc, mt->pnames);
+    CuAssertStrEquals(tc, "name", mt->pnames[0]);
+    CuAssertStrEquals(tc, "number", mt->pnames[1]);
+    CuAssertPtrNotNull(tc, mt->types);
+    CuAssertStrEquals(tc, "string", mt->types[0]->name);
+    CuAssertStrEquals(tc, "int", mt->types[1]->name);
+    test_cleanup();
+}
+
+CuSuite *get_message_suite(void)
+{
+    CuSuite *suite = CuSuiteNew();
+    SUITE_ADD_TEST(suite, test_mt_new);
+    return suite;
+}
