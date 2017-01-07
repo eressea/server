ae3bfc952b06f3d538d5f1cc7539cf64bb847e42
Enno Rehling <enno.rehling@gmail.com>
2017-01-07 21:19:58 +0100

    create_order takes variable arguments! more snprintf removal.

--- src/kernel/jsonconf.c
+++ src/kernel/jsonconf.c
@@ -801,10 +801,10 @@ static void json_settings(cJSON *json) {
         else {
             char value[32];
             if (child->type == cJSON_Number && child->valuedouble && child->valueint<child->valuedouble) {
-                _snprintf(value, sizeof(value), "%f", child->valuedouble);
+                sprintf(value, "%f", child->valuedouble);
             }
             else {
-                _snprintf(value, sizeof(value), "%d", child->valueint);
+                sprintf(value, "%d", child->valueint);
             }
             config_set(child->string, value);
         }
