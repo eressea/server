3d8d091207494a65785feb2f61e1c2fe69486f9e
Enno Rehling <enno@eressea.de>
2017-01-30 10:26:08 +0100

    new build scripts, new versioning system

--- src/kernel/version.c
+++ src/kernel/version.c
@@ -5,7 +5,7 @@
 
 #ifndef ERESSEA_VERSION
 // the version number, if it was not passed to make with -D
-#define ERESSEA_VERSION "3.10.0-devel"
+#define ERESSEA_VERSION "3.10.0"
 #endif
 
 const char *eressea_version(void) {
