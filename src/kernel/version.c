b8d9fae4e446685b2e331036b80540283401f001
Enno Rehling <enno@kn-bremen.de>
2017-02-26 20:16:22 +0100

    begin version 3.12

--- src/kernel/version.c
+++ src/kernel/version.c
@@ -5,7 +5,7 @@
 
 #ifndef ERESSEA_VERSION
 /* the version number, if it was not passed to make with -D */
-#define ERESSEA_VERSION "3.11.0"
+#define ERESSEA_VERSION "3.12.0"
 #endif
 
 const char *eressea_version(void) {
