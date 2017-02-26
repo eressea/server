f406c476574a86657b7f679ebba398366187d465
Enno Rehling <enno.rehling@gmail.com>
2017-02-26 15:30:58 +0100

    use converter frm noxml branch

--- src/convert.c
+++ src/convert.c
@@ -1,3 +1,25 @@
-int main(void) {
-    return -1;
+#include <platform.h>
+
+#include <kernel/race.h>
+#include <kernel/rules.h>
+#include <kernel/xmlreader.h>
+
+#include <races/races.h>
+
+#include <util/xml.h>
+
+#include <storage.h>
+
+int main(int argc, char **argv) {
+    const char * xmlfile, *catalog;
+
+    register_races();
+    register_xmlreader();
+
+    if (argc < 3) return -1;
+    xmlfile = argv[1];
+    catalog = argv[2];
+    read_xml(xmlfile, catalog);
+    write_rules("rules.dat");
+    return 0;
 }
