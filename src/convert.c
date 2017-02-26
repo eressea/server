71e5d101a82272d2c67d35201314cf52cf292002
Enno Rehling <enno.rehling@gmail.com>
2017-02-26 15:33:32 +0100

    converter compiles fine, does nothing

--- src/convert.c
+++ src/convert.c
@@ -1,25 +1,44 @@
 #include <platform.h>
 
+
+#ifdef USE_LIBXML2
+#include <kernel/xmlreader.h>
+#include <util/xml.h>
+#endif
 #include <kernel/race.h>
 #include <kernel/rules.h>
-#include <kernel/xmlreader.h>
-
 #include <races/races.h>
-
-#include <util/xml.h>
 
 #include <storage.h>
 
+#include <string.h>
+
+static int usage(void) {
+    return -1;
+}
+
 int main(int argc, char **argv) {
-    const char * xmlfile, *catalog;
+    const char *mode;
 
     register_races();
+#ifdef USE_LIBXML2
     register_xmlreader();
-
-    if (argc < 3) return -1;
-    xmlfile = argv[1];
-    catalog = argv[2];
-    read_xml(xmlfile, catalog);
-    write_rules("rules.dat");
-    return 0;
+#endif
+    if (argc < 2) return usage();
+    mode = argv[1];
+#ifdef USE_LIBXML2
+    if (strcmp(mode, "rules")==0) {
+        const char *xmlfile, *catalog;
+        if (argc < 4) return usage();
+        xmlfile = argv[2];
+        catalog = argv[3];
+        read_xml(xmlfile, catalog);
+        write_rules("rules.dat");
+        return 0;
+    }
+#endif
+    if (strcmp(mode, "po")==0) {
+        return 0;
+    }
+    return usage();
 }
