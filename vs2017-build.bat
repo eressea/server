77abd703bdb6b586b3dde608f6a78b1d462dce18
Enno Rehling <enno.rehling@gmail.com>
2017-03-08 20:15:41 +0100

    backfill test that demonstrates bug 2305.

---
+++ vs2017-build.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+SET VSVERSION=15
+SET SRCDIR=%CD%
+CD ..
+SET ERESSEA=%CD%
+
+CD %SRCDIR%
+IF exist build-vs%VSVERSION% goto HAVEDIR
+mkdir build-vs%VSVERSION%
+:HAVEDIR
+cd build-vs%VSVERSION%
+"%ProgramFiles%\CMake\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%SRCDIR%/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
+PAUSE
