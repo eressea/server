8d2687ab902d9d1a96346c873d832790ff9588c1
Enno Rehling <enno.rehling@gmail.com>
2017-03-29 04:30:54 +0200

    print version number at end of tests.

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -8,6 +8,7 @@ SET SERVER=%BUILD%\eressea.exe
 %SERVER% ..\scripts\run-tests.lua
 %SERVER% -re2 ..\scripts\run-tests-e2.lua
 %SERVER% -re3 ..\scripts\run-tests-e3.lua
+%SERVER% --version
 PAUSE
 RMDIR /s /q reports
 DEL score score.alliances datum turn
