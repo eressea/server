59169f3eb1f4abb23235e37ef4ff34b9896cb472
Enno Rehling <enno.rehling@gmail.com>
2017-09-21 17:26:37 +0200

    simplicate

--- src/building_action.c
+++ src/building_action.c
@@ -106,8 +106,7 @@ static int lc_read(struct attrib *a, void *owner, gamedata *data)
     building *b = (building *)owner;
     int result = 0;
     if (data->version < ATTRIBOWNER_VERSION) {
-        result = read_reference(&b, data, read_building_reference, resolve_building);
-        assert(b == owner);
+        READ_INT(data->store, NULL);
     }
     READ_TOK(store, name, sizeof(name));
     if (strcmp(name, "tunnel_action") == 0) {
