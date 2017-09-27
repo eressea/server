9372c107dc541507248b3726d886143dedd63710
Steffen Mecke <stm2@users.sf.net>
2017-09-27 10:24:01 +0200

    updating documentation

--- process/run-turn.sh
+++ process/run-turn.sh
@@ -260,17 +260,20 @@ done
 
 free_lock $LOCKFILE
 
+## TODO execute.lock unnecessary because of LOCKFILE?
+## TODO orders-*: lock_file die or unlink?
+## TODO create-oders: check exit status and die or press on?
 ## DONE install etc/procmail,fetchmail...
 ## DONE install server/Mail
 ## NOT install no runturn.lua
-## TODO procmail in requirements
-## TODO execute.lock unnecessary because of LOCKFILE?
+## (DONE) procmail in requirements
 ## DONE eressea.ini checker options , checker template
-## TODO eressea.ini email, sender, name
-## turn 0 in tutorial?
-## TODO orders-*: lock_file die or unlink?
-## TODO create-oders: check exit status and die or press on?
+## DONE eressea.ini email, sender, name
+## DONE turn 1 in tutorial?
 
+
+### these are all (?) the scripts:
+# scripts deprecated by this script are marked with *
 
 # orders-accept: called by procmail; parse email, write orders.queue and orders.dir; works with orders.queue.lock
 ## eorders.py: used by orders-process/accept; helper
@@ -286,23 +289,19 @@ free_lock $LOCKFILE
 ### checker.sh: called by orders-process; configured in eresse.ini: game.checker
 ### eorders.py
 
-# **run-eressea.cron: called by cron (or hand); waits for queue.lock, calls create-oders, backup, run-turn, sends reports
-## create-orders: called by run-eressea.cron; creates orders.$turn from orders.dir, moves orders.dir to orders.dir.$turn; works with orders.queue.lock
-## backup-eressea: called by run-eressea.cron
-## run-turn: called by run-eressea.cron; runs server once
-## compress.sh: called by run-eressea.cron; calls compress.py
+# *run-eressea.cron: called by cron (or hand); waits for queue.lock, calls create-oders, backup, run-turn, sends reports
+## *create-orders: called by run-eressea.cron; creates orders.$turn from orders.dir, moves orders.dir to orders.dir.$turn; works with orders.queue.lock
+## (*)backup-eressea: called by run-eressea.cron
+## (*) run-turn: called by run-eressea.cron; runs server once
+## (*) compress.sh: called by run-eressea.cron; calls compress.py
 ### compress.py: called by compress.sh; compresses reports of factions in reports.txt, creates 1234.sh scripts for reportnachforderung
-## sendreports.sh: called by run-eressea.cron; send all reports, uses 1234.sh in reports
+## *sendreports.sh: called by run-eressea.cron; send all reports, uses 1234.sh in reports
 ### functions.sh
 #### send-bz2-report: called by 1234.sh in reports; email one faction's report
 #### send-zip-report: more modern version
 
-#run-turn.sh: to be called by cron or hand; calls update, send (one or all, via mutt), fetchmail (and thus procmail), create_orders, write_reports, run_turn, or run_all;
+# run-turn.sh: called by cron or hand; calls update, send (one or all, via mutt), fetchmail (and thus procmail), create_orders, write_reports, run_turn, or run_all;
 
 # preview.cron: runs preview reports
 
 # tests/run-turn.sh: called by integration build
-
-
-# edit fetchmailrc, procmailrc, muttrc
-# 
