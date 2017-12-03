4619f77af9dd98e0be906c97857c6f7c816d95c4
Enno Rehling <enno.rehling@gmail.com>
2017-12-03 15:43:22 +0100

    add a shellcheck test, fix some scripts

--- process/sendreports.sh
+++ process/sendreports.sh
@@ -3,30 +3,23 @@
 ##
 ## Prepare the report
 
-if [ -z $ERESSEA ]; then
+if [ -z "$ERESSEA" ]; then
   echo "You have to define the \$ERESSEA environment variable to run $0"
   exit -2
 fi
-source $HOME/bin/functions.sh
 
-if [ ! -z $1 ]; then
-  GAME=$ERESSEA/game-$1
+if [ ! -z "$1" ]; then
+  GAME="$ERESSEA/game-$1"
 else
   GAME=$ERESSEA
 fi
 
-cd $GAME/reports || abort "could not chdir to reports directory"
+cd "$GAME/reports" || exit
 for REPORT in *.sh
 do
   echo -n "Sending "
-  basename $REPORT .sh
-  bash $REPORT
+  basename "$REPORT" .sh
+  bash "$REPORT"
 done
-cd -
-
-if [ -e $GAME/ages.sh ]; then
-  cd $GAME
-  ./ages.sh
-  cd -
-fi
+cd - || exit
 
