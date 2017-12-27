4018850499fdc5d67c03716f65a809c873fbad9f
Enno Rehling <enno.rehling@gmail.com>
2017-12-27 23:47:59 +0100

    wtf is up with the travis build now?

--- src/util/bsdstring.c
+++ src/util/bsdstring.c
@@ -40,7 +40,8 @@ size_t strlcpy(char *dst, const char *src, size_t siz)
     register const char *s = src;
     register size_t n = siz;
 
-    assert(src && dst);
+    assert(src);
+    assert(dst);
     /* Copy as many bytes as will fit */
     if (n != 0 && --n != 0) {
         do {
