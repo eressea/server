7affbd6c74a9ebebf8d949a2b881e607a423a4ea
Enno Rehling <enno.rehling@gmail.com>
2018-07-09 03:31:13 +0200

    autostudy continued (WIP)

--- src/automate.c
+++ src/automate.c
@@ -13,9 +13,10 @@
 
 #include <stdlib.h>
 
-int cmp_students(const void *lhs, const void *rhs) {
-    const student *a = (const student *)lhs;
-    const student *b = (const student *)rhs;
+int cmp_scholars(const void *lhs, const void *rhs)
+{
+    const scholar *a = (const scholar *)lhs;
+    const scholar *b = (const scholar *)rhs;
     if (a->sk == b->sk) {
         /* sort by level, descending: */
         return b->level - a->level;
@@ -24,17 +25,17 @@ int cmp_students(const void *lhs, const void *rhs) {
     return (int)a->sk - (int)b->sk;
 }
 
-int autostudy_init(student students[], int max_students, region *r)
+int autostudy_init(scholar scholars[], int max_scholars, region *r)
 {
     unit *u;
-    int nstudents = 0;
+    int nscholars = 0;
 
     for (u = r->units; u; u = u->next) {
         keyword_t kwd = getkeyword(u->thisorder);
         if (kwd == K_AUTOSTUDY) {
-            student * st = students + nstudents;
-            if (++nstudents == max_students) {
-                log_fatal("you must increase MAXSTUDENTS");
+            scholar * st = scholars + nscholars;
+            if (++nscholars == max_scholars) {
+                log_fatal("you must increase MAXSCHOLARS");
             }
             st->u = u;
             init_order(u->thisorder, u->faction->locale);
@@ -43,31 +44,73 @@ int autostudy_init(student students[], int max_students, region *r)
             st->learn = 0;
         }
     }
-    if (nstudents > 0) {
-        qsort(students, nstudents, sizeof(student), cmp_students);
+    if (nscholars > 0) {
+        qsort(scholars, nscholars, sizeof(scholar), cmp_scholars);
     }
-    return nstudents;
+    return nscholars;
 }
 
-#define MAXSTUDENTS 128
+void autostudy_run(scholar scholars[], int nscholars)
+{
+    int i, t, s, ti = 0, si = 0, ts = 0, tt = 0;
+    skill_t sk = scholars[0].sk;
+    for (i = ti; i != nscholars && scholars[i].sk == sk; ++i) {
+        int mint;
+        ts += scholars[i].u->number; /* count total scholars */
+        mint = (ts + 10) / 11; /* need a minimum of ceil(ts/11) teachers */
+        while (mint>tt) {
+            tt += scholars[si++].u->number;
+        }
+    }
+    /* now si splits the teachers and students 1:10 */
+    /* first student must be 2 levels below first teacher: */
+    while (scholars[ti].level - TEACHDIFFERENCE > scholars[si].level) {
+        tt += scholars[si++].u->number;
+    }
+    /* invariant: unit ti can still teach i students */
+    i = scholars[ti].u->number * 10;
+    for (t = ti, s = si; t != si && s != nscholars; ++t) {
+        /* TODO: is there no constant for students per teacher? */
+        while (s != nscholars) {
+            int n = scholars[s].u->number;
+            scholars[s].learn += n;
+            if (i >= n) {
+                i -= n;
+                scholars[s].learn += n;
+                /* next student */
+            }
+            else {
+                scholars[s].learn += i;
+                /* go to next suitable teacher */
+                do {
+                    ++t;
+                }
+                while (scholars[t].level - TEACHDIFFERENCE < scholars[s].level);
+            }
+        }
+    }
+}
 
-void do_autostudy(region *r) {
-    student students[MAXSTUDENTS];
-    int nstudents = autostudy_init(students, MAXSTUDENTS, r);
+#define MAXSCHOLARS 128
 
-    if (nstudents > 0) {
+void do_autostudy(region *r)
+{
+    scholar scholars[MAXSCHOLARS];
+    int nscholars = autostudy_init(scholars, MAXSCHOLARS, r);
+
+    if (nscholars > 0) {
         int i;
         skill_t sk = NOSKILL;
 
-        for (i = 0; i != nstudents; ++i) {
-            if (students[i].u) {
+        for (i = 0; i != nscholars; ++i) {
+            if (scholars[i].u) {
                 if (sk == NOSKILL) {
-                    sk = students[i].sk;
+                    sk = scholars[i].sk;
                 }
-                else if (sk != students[i].sk) {
+                else if (sk != scholars[i].sk) {
                     continue;
                 }
-                students[i].u = NULL;
+                scholars[i].u = NULL;
             }
         }
     }
--- src/automate.test.c
+++ src/automate.test.c
@@ -13,8 +13,8 @@
 
 #include <CuTest.h>
 
-static void test_autostudy(CuTest *tc) {
-    student students[4];
+static void test_autostudy_init(CuTest *tc) {
+    scholar scholars[4];
     unit *u1, *u2, *u3;
     faction *f;
     region *r;
@@ -30,27 +30,55 @@ static void test_autostudy(CuTest *tc) {
     set_level(u2, SK_ENTERTAINMENT, 2);
     u3 = test_create_unit(f, r);
     u3->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_PERCEPTION]);
-    students[3].u = NULL;
-    CuAssertIntEquals(tc, 3, autostudy_init(students, 4, r));
-    CuAssertPtrEquals(tc, u2, students[0].u);
-    CuAssertIntEquals(tc, 2, students[0].level);
-    CuAssertIntEquals(tc, 0, students[0].learn);
-    CuAssertIntEquals(tc, SK_ENTERTAINMENT, students[0].sk);
-    CuAssertPtrEquals(tc, u1, students[1].u);
-    CuAssertIntEquals(tc, 0, students[1].level);
-    CuAssertIntEquals(tc, 0, students[1].learn);
-    CuAssertIntEquals(tc, SK_ENTERTAINMENT, students[1].sk);
-    CuAssertPtrEquals(tc, u3, students[2].u);
-    CuAssertIntEquals(tc, 0, students[2].level);
-    CuAssertIntEquals(tc, 0, students[2].learn);
-    CuAssertIntEquals(tc, SK_PERCEPTION, students[2].sk);
-    CuAssertPtrEquals(tc, NULL, students[3].u);
+    scholars[3].u = NULL;
+    CuAssertIntEquals(tc, 3, autostudy_init(scholars, 4, r));
+    CuAssertPtrEquals(tc, u2, scholars[0].u);
+    CuAssertIntEquals(tc, 2, scholars[0].level);
+    CuAssertIntEquals(tc, 0, scholars[0].learn);
+    CuAssertIntEquals(tc, SK_ENTERTAINMENT, scholars[0].sk);
+    CuAssertPtrEquals(tc, u1, scholars[1].u);
+    CuAssertIntEquals(tc, 0, scholars[1].level);
+    CuAssertIntEquals(tc, 0, scholars[1].learn);
+    CuAssertIntEquals(tc, SK_ENTERTAINMENT, scholars[1].sk);
+    CuAssertPtrEquals(tc, u3, scholars[2].u);
+    CuAssertIntEquals(tc, 0, scholars[2].level);
+    CuAssertIntEquals(tc, 0, scholars[2].learn);
+    CuAssertIntEquals(tc, SK_PERCEPTION, scholars[2].sk);
+    CuAssertPtrEquals(tc, NULL, scholars[3].u);
     test_teardown();
+}
+
+static void test_autostudy_run(CuTest *tc) {
+    scholar scholars[4];
+    unit *u1, *u2, *u3;
+    faction *f;
+    region *r;
+
+    test_setup();
+    r = test_create_plain(0, 0);
+    f = test_create_faction(NULL);
+    u1 = test_create_unit(f, r);
+    u1->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_ENTERTAINMENT]);
+    set_number(u1, 2);
+    u2 = test_create_unit(f, r);
+    u2->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_ENTERTAINMENT]);
+    set_number(u2, 10);
+    set_level(u2, SK_ENTERTAINMENT, 2);
+    u3 = test_create_unit(f, r);
+    u3->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_PERCEPTION]);
+    set_number(u3, 20);
+    scholars[3].u = NULL;
+    CuAssertIntEquals(tc, 3, autostudy_init(scholars, 4, r));
+    autostudy_run(scholars, 3);
+    CuAssertIntEquals(tc, 0, scholars[0].learn);
+    CuAssertIntEquals(tc, 20, scholars[1].learn);
+    CuAssertIntEquals(tc, 30, scholars[2].learn);
 }
 
 CuSuite *get_automate_suite(void)
 {
     CuSuite *suite = CuSuiteNew();
-    SUITE_ADD_TEST(suite, test_autostudy);
+    SUITE_ADD_TEST(suite, test_autostudy_init);
+    SUITE_ADD_TEST(suite, test_autostudy_run);
     return suite;
 }
