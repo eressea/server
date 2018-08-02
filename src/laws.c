116ff7247dcde0be4334748f47b555f7c11022df
Enno Rehling <enno.rehling@gmail.com>
2018-08-02 16:41:23 +0200

    Remove unused "message" events (old NPC sh*t).

--- src/laws.c
+++ src/laws.c
@@ -2058,17 +2058,6 @@ int mail_cmd(unit * u, struct order *ord)
                 break;
             }
             else {
-                attrib *a = a_find(u2->attribs, &at_eventhandler);
-                if (a != NULL) {
-                    event_arg args[3];
-                    args[0].data.v = (void *)s;
-                    args[0].type = "string";
-                    args[1].data.v = (void *)u;
-                    args[1].type = "unit";
-                    args[2].type = NULL;
-                    handle_event(a, "message", args);
-                }
-
                 mailunit(r, u, n, ord, s);
             }
             return 0;
