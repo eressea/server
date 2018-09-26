53afc86e009deef9cb4d5c43a65df6e7a2c8b9a1
Enno Rehling <enno@eressea.de>
2018-09-26 17:09:29 +0200

    fix bcrypt library, add to password.c (works on mac)

--- src/util/password.c
+++ src/util/password.c
@@ -1,18 +1,39 @@
+#ifdef _MSC_VER
 #include <platform.h>
+#endif
 #include "password.h"
+
+#include <bcrypt.h>
 
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
 
-bool password_is_implemented(int algo) {
+bool password_is_implemented(cryptalgo_t algo) {
+    if (algo == PASSWORD_BCRYPT) return true;
     return algo == PASSWORD_PLAINTEXT;
 }
 
-const char * password_encode(const char * passwd, int algo) {
+const char * password_encode(const char * passwd, cryptalgo_t algo) {
+    if (algo == PASSWORD_BCRYPT) {
+        char salt[BCRYPT_HASHSIZE];
+        static char hash[BCRYPT_HASHSIZE];
+        int ret;
+        bcrypt_gensalt(12, salt);
+        ret = bcrypt_hashpw(passwd, salt, hash);
+        assert(ret == 0);
+        return hash;
+    }
     return passwd;
 }
 
 int password_verify(const char * pwhash, const char * passwd) {
+    if (pwhash[0] == '$') {
+        if (pwhash[1] == '2') {
+            int ret = bcrypt_checkpw(passwd, pwhash);
+            assert(ret != -1);
+            return (ret == 0) ? VERIFY_OK : VERIFY_FAIL;
+        }
+    }
     return (strcmp(passwd, pwhash) == 0) ? VERIFY_OK : VERIFY_FAIL;
 }
--- src/util/password.h
+++ src/util/password.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <stdbool.h>
-#define PASSWORD_PLAINTEXT 0
+typedef enum cryptalgo_t {
+    PASSWORD_PLAINTEXT,
+    PASSWORD_BCRYPT
+} cryptalgo_t;
 #define PASSWORD_DEFAULT PASSWORD_PLAINTEXT
 
 #define VERIFY_OK 0
 #define VERIFY_FAIL 1
 #define VERIFY_UNKNOWN 2
 int password_verify(const char *hash, const char *passwd);
-const char * password_encode(const char *passwd, int algo);
-bool password_is_implemented(int algo);
+const char * password_encode(const char *passwd, cryptalgo_t algo);
+bool password_is_implemented(cryptalgo_t algo);
