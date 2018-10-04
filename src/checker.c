9a1bdf444439d0dfb7d18f0c37d3bea6b26a6f00
Enno Rehling <enno@eressea.de>
2018-10-04 10:49:53 +0200

    fix checker build and arg parsing

--- src/checker.c
+++ src/checker.c
@@ -4,6 +4,7 @@
 
 #include "util/order_parser.h"
 
+#include <errno.h>
 #include <stdio.h>
 
 typedef struct parser_state {
@@ -48,7 +49,7 @@ int parsefile(FILE *F) {
 
 int main(int argc, char **argv) {
     FILE * F = stdin;
-    if (argc >= 1) {
+    if (argc > 1) {
         const char *filename = argv[1];
         F = fopen(filename, "r");
         if (!F) {
