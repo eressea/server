272588cb0f3771b041c23eeaf0056a2a6e150194
Enno Rehling <enno.rehling@gmail.com>
2018-11-02 22:31:20 +0100

    BUG 2509: fix travelthru faction reporting

--- src/reports.c
+++ src/reports.c
@@ -916,7 +916,7 @@ bufunit(const faction * f, const unit * u, seen_mode mode, char *buf,
         }
         if (!isbattle) {
             int printed = 0;
-            order *ord;;
+            order *ord;
             for (ord = u->old_orders; ord; ord = ord->next) {
                 keyword_t kwd = getkeyword(ord);
                 if (is_repeated(kwd)) {
@@ -1133,7 +1133,6 @@ static void cb_add_address(region *r, unit *ut, void *cbdata) {
             if (data->lastf != sf && cansee_unit(ut, u, data->stealthmod)) {
                 add_seen_faction_i(data->flist, sf);
                 data->lastf = sf;
-                break;
             }
         }
     }
