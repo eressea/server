ed17b1028702e040633e3da67e324208a0e361f4
Enno Rehling <enno.rehling@gmail.com>
2018-11-06 19:28:14 +0100

    Bug 2514: units giving LERNE AUTO with certain incomplete teacher/student matchups did not study anything.

--- src/automate.test.c
+++ src/automate.test.c
@@ -91,7 +91,7 @@ static void test_autostudy_run(CuTest *tc) {
     CuAssertIntEquals(tc, 3, autostudy_init(scholars, 4, &ulist));
     CuAssertPtrEquals(tc, NULL, ulist);
     autostudy_run(scholars, 3);
-    CuAssertIntEquals(tc, 0, scholars[0].learn);
+    CuAssertIntEquals(tc, 1, scholars[0].learn);
     CuAssertIntEquals(tc, 20, scholars[1].learn);
     CuAssertIntEquals(tc, 15, scholars[2].learn);
     test_teardown();
@@ -127,11 +127,46 @@ static void test_autostudy_run_noteachers(CuTest *tc) {
     test_teardown();
 }
 
+/**
+ * Reproduce Bug 2514
+ */
+static void test_autostudy_run_skilldiff(CuTest *tc) {
+    scholar scholars[4];
+    unit *u1, *u2, *u3, *ulist;
+    faction *f;
+    region *r;
+
+    test_setup();
+    r = test_create_plain(0, 0);
+    f = test_create_faction(NULL);
+    u1 = test_create_unit(f, r);
+    u1->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_PERCEPTION]);
+    set_number(u1, 1);
+    set_level(u1, SK_PERCEPTION, 2);
+    u2 = test_create_unit(f, r);
+    u2->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_PERCEPTION]);
+    set_number(u2, 10);
+    set_level(u2, SK_PERCEPTION, 1);
+    u3 = test_create_unit(f, r);
+    u3->thisorder = create_order(K_AUTOSTUDY, f->locale, skillnames[SK_PERCEPTION]);
+    set_number(u3, 10);
+    scholars[3].u = NULL;
+    ulist = r->units;
+    CuAssertIntEquals(tc, 3, autostudy_init(scholars, 4, &ulist));
+    CuAssertPtrEquals(tc, NULL, ulist);
+    autostudy_run(scholars, 3);
+    CuAssertIntEquals(tc, 0, scholars[0].learn);
+    CuAssertIntEquals(tc, 20, scholars[2].learn);
+    CuAssertIntEquals(tc, 10, scholars[1].learn);
+    test_teardown();
+}
+
 CuSuite *get_automate_suite(void)
 {
     CuSuite *suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, test_autostudy_init);
     SUITE_ADD_TEST(suite, test_autostudy_run);
     SUITE_ADD_TEST(suite, test_autostudy_run_noteachers);
+    SUITE_ADD_TEST(suite, test_autostudy_run_skilldiff);
     return suite;
 }
