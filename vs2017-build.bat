65dee13592a38915c3fa329ee6241ed80eb68fcb
Enno Rehling <enno.rehling@gmail.com>
2019-04-06 11:29:19 +0200

    I am moving my library dependency folder to c:\libraries build.bat needed to know that.

--- vs2017-build.bat
+++ vs2017-build.bat
@@ -1,13 +1,16 @@
 @ECHO OFF
+IF "%WIN32_DEV%" == "" SET WIN32_DEV="C:\Libraries"
+SET CMAKE_ROOT=%ProgramFiles%\CMake
+IF "%LUA_DEV%" == "" SET LUA_DEV="%ProgramFiles(x86)%/Lua/5.1"
 SET VSVERSION=15
 SET SRCDIR=%CD%
-CD ..
-SET ERESSEA=%CD%
+REM CD ..
+REM SET ERESSEA=%CD%
+REM CD %SRCDIR%
 
-CD %SRCDIR%
 IF exist build-vs%VSVERSION% goto HAVEDIR
 mkdir build-vs%VSVERSION%
 :HAVEDIR
 cd build-vs%VSVERSION%
-"%ProgramFiles%\CMake\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%SRCDIR%/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
+"%CMAKE_ROOT%\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%LUA_DEV%;%WIN32_DEV%" -DCMAKE_MODULE_PATH="%SRCDIR%/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
 PAUSE
