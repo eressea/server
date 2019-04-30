8c0fc4bd918610edabded2bb2da3c92f164aeaa2
Enno Rehling <enno.rehling@gmail.com>
2019-04-30 18:56:37 +0200

    2577: Fix resource values, again.

--- src/kernel/gamedata.h
+++ src/kernel/gamedata.h
@@ -42,8 +42,9 @@
 #define FAMILIAR_FIXMAGE_VERSION 364 /* familiar links are fixed */
 #define FAMILIAR_FIXSPELLBOOK_VERSION 365 /* familiar spells are fixed */
 #define FIX_STARTLEVEL_VERSION 366 /* fixing resource startlevels */
+#define FIX_RES_BASE_VERSION 367 /* fixing resource base */
 
-#define RELEASE_VERSION FIX_STARTLEVEL_VERSION /* current datafile */
+#define RELEASE_VERSION FIX_RES_BASE_VERSION /* current datafile */
 #define MIN_VERSION UIDHASH_VERSION      /* minimal datafile we support */
 #define MAX_VERSION RELEASE_VERSION /* change this if we can need to read the future datafile, and we can do so */
 
