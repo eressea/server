2ef753bd161ab9b080e7b278c104e133594d40a2
Enno Rehling <enno.rehling@gmail.com>
2019-09-24 21:42:53 +0200

    Tod den region_list Listen. Weg mit astralregions().

--- src/teleport.h
+++ src/teleport.h
@@ -16,8 +16,6 @@ extern "C" {
 
     struct region *r_standard_to_astral(const struct region *r);
     struct region *r_astral_to_standard(const struct region *);
-    struct region_list *astralregions(const struct region *rastral,
-        bool(*valid) (const struct region *));
     struct region_list *all_in_range(const struct region *r, int n,
         bool(*valid) (const struct region *));
     bool inhabitable(const struct region *r);
