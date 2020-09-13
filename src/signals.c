7eb26db2ec39185b6d9ca1c8d4359d5b0cd91143
Enno Rehling <enno.rehling@gmail.com>
2020-09-13 20:34:45 +0200

    move gcc-specific signal handling out of main.c

---
+++ src/signals.c
@@ -0,0 +1,36 @@
+#ifdef HAVE_BACKTRACE
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <bits/types/siginfo_t.h>
+static void *btrace[50];
+
+static void report_segfault(int signo, siginfo_t * sinf, void *arg)
+{
+    size_t size;
+    int fd = fileno(stderr);
+
+    fflush(stdout);
+    fputs("\n\nProgram received SIGSEGV, backtrace follows.\n", stderr);
+    size = backtrace(btrace, 50);
+    backtrace_symbols_fd(btrace, size, fd);
+    abort();
+}
+
+int setup_signal_handler(void)
+{
+    struct sigaction act;
+
+    act.sa_flags = SA_RESETHAND | SA_SIGINFO;
+    act.sa_sigaction = report_segfault;
+    sigfillset(&act.sa_mask);
+    return sigaction(SIGSEGV, &act, 0);
+}
+#else
+int setup_signal_handler(void)
+{
+    return 0;
+}
+#endif
+
