f69a823a1412ef17a5112f205a5608f5166e7c69
Enno Rehling <enno@eressea.de>
2021-02-22 01:59:41 +0100

    fix LUA_PATH for runtests script

--- scripts/eressea/path.lua
+++ scripts/eressea/path.lua
@@ -10,8 +10,3 @@ else
 end
 path = path .. "/scripts"
 package.path = path .. '/?.lua;' .. path .. '/?/init.lua;' .. package.path
-if config.paths ~= nil then
-    for path in string.gmatch(config.paths, "([^:]+)") do
-        package.path = package.path .. ';' .. path .. '/?.lua;' .. path .. '/?/init.lua'
-    end
-end
