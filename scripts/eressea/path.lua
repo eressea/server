e36cac1188b42041c454ba2f4aeb09a053cf3f5f
Enno Rehling <enno@eressea.de>
2021-02-23 17:19:48 +0100

    find config files in ERESSEA_DIR if config.install is not set

--- scripts/eressea/path.lua
+++ scripts/eressea/path.lua
@@ -1,11 +1,8 @@
-local path = os.getenv("ERESSEA_DIR")
-if not path then
-    if config.install then
-	    path = config.install
-    else
-        path = '.'
-    end
+local path = '.'
+if config.install then
+	path = config.install
 else
+    path = os.getenv("ERESSEA_DIR") or path
     config.install = path
 end
 path = path .. "/scripts"
