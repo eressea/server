09f223e8272466a6bf7a61c52729264750b70421
Steffen Mecke <stm2@users.sf.net>
2021-08-13 16:46:02 +0200

    empty unit stealth tests

--- scripts/tests/recruit.lua
+++ scripts/tests/recruit.lua
@@ -77,3 +77,47 @@ function test_guarded_temp_cannot_recruit()
     assert_equal(0, zero)
     assert_equal(1, count)
 end
+
+function test_recruit_empty()
+    local r = region.create(0, 0, 'plain')
+
+    local f1 = faction.create('human', "recruit1@eressea.de", "de")
+    local f2 = faction.create('human', "recruit2@eressea.de", "de")
+    local u1 = unit.create(f1, r, 1)
+    local u11 = unit.create(f1, r, 1)
+    local u2 = unit.create(f2, r, 1)
+
+    r.peasants = 1000
+
+    u2:add_item("sword", 1)
+    u2:set_skill("melee", 1)
+    u2.guard = true
+
+    u1:add_item("money", 100)
+    u1:add_order("MACHE TEMP x")
+    u1:add_order("ENDE")
+    u1:add_order("MACHE TEMP y")
+    u1:add_order("REKRUTIERE 1")
+    u1:add_order("ENDE")
+    u1:add_order("GIB TEMP x 1 Personen")
+
+    u11:add_item("money", 100)
+    u11:add_order("GIB 0 ALLES PERSONEN")
+    u11:add_order("REKRUTIERE 1")
+
+    process_orders()
+
+    assert_equal(0, u11.number)
+
+    local count = 0
+    local zero = 0
+    for u in f1.units do
+      if u.number == 0 then zero = zero +1 end
+      count = count +1
+    end
+    assert_equal(0, zero)
+    assert_equal(1, count)
+
+    init_reports()
+    write_reports()
+end
--- src/laws.test.c
+++ src/laws.test.c
@@ -2016,6 +2016,32 @@ static void test_cansee_temp(CuTest* tc) {
     test_teardown();
 }
 
+static void test_cansee_empty(CuTest *tc) {
+    unit *u, *u2, *utemp;
+    faction *f;
+
+    test_setup();
+    u = test_create_unit(f = test_create_faction(), test_create_region(0, 0, NULL));
+    u2 = test_create_unit(test_create_faction(), u->region);
+
+    CuAssertTrue(tc, cansee(u->faction, u->region, u2, 0));
+
+    u2->number = 0;
+    set_level(u2, SK_STEALTH, 1);
+    CuAssertTrue(tc, cansee(u->faction, u->region, u2, 0));
+
+    u2->number = 1;
+    u->orders = create_order(K_MAKETEMP, f->locale, "1");
+    new_units();
+    utemp = u2->next;
+
+    CuAssertPtrNotNull(tc, utemp);
+    CuAssertTrue(tc, cansee(u->faction, u->region, utemp, 0));
+
+    test_teardown();
+}
+
+
 /**
  * Hidden monsters are seen in oceans if they are big enough.
  */
@@ -2526,6 +2552,7 @@ CuSuite *get_laws_suite(void)
     SUITE_ADD_TEST(suite, test_cansee_ring);
     SUITE_ADD_TEST(suite, test_cansee_sphere);
     SUITE_ADD_TEST(suite, test_cansee_monsters);
+    SUITE_ADD_TEST(suite, test_cansee_empty);
     SUITE_ADD_TEST(suite, test_cansee_temp);
     SUITE_ADD_TEST(suite, test_nmr_timeout);
     SUITE_ADD_TEST(suite, test_long_orders);
