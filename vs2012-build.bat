37f35304249445663d05061c0b8ac304894f6513
Enno Rehling <enno@eressea.de>
2021-11-27 19:00:21 +0100

    Remove the .bat files, explain how to use directly mith CMake instead.

--- vs2012-build.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET VSVERSION=11
-SET SRCDIR=%CD%
-CD ..
-SET ERESSEA=%CD%
-
-mkdir build-vs%VSVERSION%
-cd build-vs%VSVERSION%
-"%ProgramFiles(x86)%\CMake\bin\cmake.exe" -G "Visual Studio %VSVERSION%" -DCMAKE_PREFIX_PATH="%ProgramFiles(x86)%/Lua/5.1;%ERESSEA%/dependencies-win32" -DCMAKE_MODULE_PATH="%ERESSEA%/server/cmake/Modules" -DCMAKE_SUPPRESS_REGENERATION=TRUE ..
