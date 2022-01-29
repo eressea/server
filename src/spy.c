adfc12d0118b4dd42f8257828a13b7cd8812a2b9
Enno Rehling <enno@eressea.de>
2022-01-29 15:37:36 +0100

    Bugfix 2765: Spionage zeigt falschen Kampfstatus.

--- src/spy.c
+++ src/spy.c
@@ -51,7 +51,7 @@ void spy_message(int spy, const unit * u, const unit * target)
     sbstring sbs;
 
     sbs_init(&sbs, status, sizeof(status));
-    report_status(u, u->faction->locale, &sbs);
+    report_status(target, u->faction->locale, &sbs);
 
     ADDMSG(&u->faction->msgs, msg_message("spyreport", "spy target status", u,
         target, status));
