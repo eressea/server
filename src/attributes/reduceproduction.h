66bbfbb30e0d5cdbac15dd667848fcc364605f43
Enno Rehling <enno@eressea.de>
2022-02-27 11:55:04 +0100

    Update version number for the Q2 release.

--- src/attributes/reduceproduction.h
+++ src/attributes/reduceproduction.h
@@ -1,13 +1,11 @@
+#pragma once
 #ifndef H_ATTRIBUTE_REDUCEPRODUCTION
 #define H_ATTRIBUTE_REDUCEPRODUCTION
-#ifdef __cplusplus
-extern "C" {
-#endif
 
-    extern struct attrib *make_reduceproduction(int percent, int time);
-    extern struct attrib_type at_reduceproduction;
+extern struct attrib_type at_reduceproduction;
 
-#ifdef __cplusplus
-}
-#endif
+struct attrib;
+
+struct attrib *make_reduceproduction(int percent, int time);
+
 #endif
