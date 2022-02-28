1d73c33bc5b26d39851cdf8affea2fe945388086
Enno Rehling <enno@eressea.de>
2022-02-28 04:52:50 +0100

    new coverity errors

--- src/kernel/race.c
+++ src/kernel/race.c
@@ -152,7 +152,6 @@ const struct race *findrace(const char *s, const struct locale *lang)
 const struct race *get_race(race_t rt) {
     const char * name;
 
-    assert(rt >= 0);
     assert(rt < MAXRACES);
     name = racenames[rt];
     if (!name) {
