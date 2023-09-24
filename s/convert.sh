5273d665aba47822220b6472f43221fd2f2de606
Steffen Mecke <stm2@users.sf.net>
2023-09-24 14:04:57 +0200

    safer variables (by shellcheck)

--- s/convert.sh
+++ s/convert.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
-while [ ! -z $1 ] ; do
-        tmpfile=$(mktemp eressea.XXX)
-        iconv -f latin1 -t utf-8 < $1 | \
-                perl -pe 's/ß/ss/; s/ä/ae/; s/ü/ue/; s/ö/oe/;' \
-                > $tmpfile && \mv $tmpfile $1
-    file $1
-        shift 1
+#!/bin/bash
+set -euo pipefail
+
+while [ -n "${1+x}" ] ; do
+    tmpfile=$(mktemp eressea.XXX)
+    iconv -f latin1 -t utf-8 < "$1" | \
+        perl -pe 's/ß/ss/; s/ä/ae/; s/ü/ue/; s/ö/oe/;' \
+        > "$tmpfile" && \mv "$tmpfile" "$1"
+    file "$1"
+    shift 1
 done
