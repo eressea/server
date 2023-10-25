ea48671269e7df153185783e405f27ee422c99dd
Enno Rehling <enno@eressea.de>
2023-10-25 13:44:36 +0200

    iwyu, stdarg.h

--- src/tests.c
+++ src/tests.c
@@ -49,6 +49,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdbool.h>           // for true
+#include <stdarg.h>
 #include <stdio.h>             // for fprintf, stderr
 #include <stdlib.h>
 #include <string.h>
