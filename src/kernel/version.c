93a2faac4d472357c14247cab9d3220b7dd736ae
Enno Rehling <enno@eressea.de>
2024-09-09 09:36:39 +0200

    begin development of 29.4

--- src/kernel/version.c
+++ src/kernel/version.c
@@ -8,7 +8,7 @@
 
 #ifndef ERESSEA_VERSION
 /* the version number, if it was not passed to make with -D */
-#define ERESSEA_VERSION "29.3.0"
+#define ERESSEA_VERSION "29.4.0"
 #endif
 
 const char *eressea_version(void) {
