f06ae84f3307f25041b79808d9d70035b60930a6
Enno Rehling <enno.rehling@gmail.com>
2025-02-19 19:49:05 +0100

    try using dlmalloc for stb_ds (failing)

--- src/util/stb.c
+++ src/util/stb.c
@@ -1,4 +1,12 @@
+#define USE_DL_PREFIX
+#define DL_MALLOC_IMPLEMENTATION
+#include <dlmalloc.h>
+#define STBDS_REALLOC(c,p,s) dlrealloc(p,s)
+#define STBDS_FREE(c,p)      dlfree(p)
 #define STB_DS_IMPLEMENTATION
 #include <stb_ds.h>
+#define GB_ALLOC(s) dlmalloc(s)
+#define GB_FREE(p) dlfree(p)
 #define GB_STRING_IMPLEMENTATION
 #include <gb_string.h>
+
