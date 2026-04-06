115589a710994ecd5be0e40d454c0aab1200dee8
Enno Rehling <enno.rehling@gmail.com>
2026-04-06 14:42:37 +0200

    script to create a minimal game
    some funny experimental script magic

---
+++ scripts/newgame.lua
@@ -0,0 +1,14 @@
+require 'config'
+--require 'eressea.path'
+--require 'eressea'
+--require 'eressea.xmlconf'
+
+region.create(0, 0, 'plain')
+region.create(1, 0, 'ocean')
+region.create(-1, 0, 'ocean')
+region.create(-1, 1, 'ocean')
+region.create(0, 1, 'ocean')
+region.create(0, -1, 'ocean')
+region.create(1, -1, 'ocean')
+
+eressea.write_game(get_turn() .. '.dat')
