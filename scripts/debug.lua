b9cf842d6fb14ba78aef02596445a0417bec7e52
Enno Rehling <enno@eressea.de>
2026-04-26 12:18:19 +0200

    debug the lua tests, fix CTest environment

--- scripts/debug.lua
+++ scripts/debug.lua
@@ -1,13 +1,5 @@
-require 'eressea.path'
-require 'eressea'
-require 'eressea.xmlconf'
-
-function gmtool_on_keypressed(keycode)
-    local warp = require('eressea.warpgate')
-    if keycode == gmtool.KEY_F1 then
-        warp.gm_edit()
-    end
-end
-
-eressea.read_game(get_turn() .. ".dat")
-dump_unit("jiha")
+print(os.getenv('LUA_PATH'))
+print(os.getenv('LUA_CPATH'))
+print(package.path)
+require 'lunitx'
+require 'lunit'
