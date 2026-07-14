52ff088ac94a0b0657e983376389b0104ffa7a2e
Enno Rehling <enno@eressea.de>
2026-07-14 19:06:47 +0200

    forward-declared structs fix compile, pragma once

--- src/randenc.h
+++ src/randenc.h
@@ -1,20 +1,14 @@
-#ifndef H_GC_RANDENC
-#define H_GC_RANDENC
-#ifdef __cplusplus
-extern "C" {
-#endif
+#pragma once
 
-    struct region;
+struct attrib;
+struct region;
+struct unit;
 
-    /** Plagues **/
+/** Plagues **/
 #define PLAGUE_CHANCE      0.1 /* Seuchenwahrscheinlichkeit (siehe plagues()) */
 #define PLAGUE_VICTIMS     0.2 /* % Betroffene */
 #define PLAGUE_HEALCHANCE  0.25        /* Wahrscheinlichkeit Heilung */
 #define PLAGUE_HEALCOST    30   /* Heilkosten */
-    void plagues(struct region *r);
-    void randomevents(void);
-    void dissolve_unit(struct unit *u, int n, const struct attrib *a);
-#ifdef __cplusplus
-}
-#endif
-#endif
+void plagues(struct region *r);
+void randomevents(void);
+void dissolve_unit(struct unit *u, int n, const struct attrib *a);
